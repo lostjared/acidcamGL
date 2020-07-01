95590cf6a07fbb94dae918968b1ccfc1d50c9bae
Jared Bruni <jaredbruni@gmail.com>
2020-06-30 23:53:02 -0700

    updated glsl code

---
+++ source/filters/cos_sin.glsl
@@ -0,0 +1,34 @@
+
+#version 410
+in vec2 tc;
+out vec4 color;
+in float alpha_r;
+in float alpha_g;
+in float alpha_b;
+in float current_index;
+in float timeval;
+
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform sampler2D samp;
+uniform float value_alpha_r, value_alpha_g, value_alpha_b;
+uniform float index_value;
+uniform float time_f;
+
+void main(void)
+{
+    color = texture(samp, tc);
+    ivec3 source;
+    for(int i = 0; i < 3; ++i) {
+        source[i] = int(255 * color[i]);
+    }
+    color[0] += timeval*cos(0.1489*tc[0]);
+    color[1] += timeval*sin(0.1482*tc[1]);
+    ivec3 int_color;
+    for(int i = 0; i < 3; ++i) {
+        int_color[i] = int(255 * color[i]);
+        int_color[i] = int_color[i]%255;
+        color[i] = float(int_color[i])/255;
+    }
+}
+
