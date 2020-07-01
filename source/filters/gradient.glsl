06890b94f9ec953fcb6049aeec5c0facbe234b71
Jared Bruni <jaredbruni@gmail.com>
2020-06-30 23:48:36 -0700

    updated

---
+++ source/filters/gradient.glsl
@@ -0,0 +1,36 @@
+
+#version 410
+in vec2 tc;
+out vec4 color;
+in float alpha_r;
+in float alpha_g;
+in float alpha_b;
+in float current_index;
+in float timeval;
+
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform sampler2D samp;
+uniform float value_alpha_r, value_alpha_g, value_alpha_b;
+uniform float index_value;
+uniform float time_f;
+
+void main(void)
+{
+    color = texture(samp, tc);
+    ivec3 source;
+    for(int i = 0; i < 3; ++i) {
+        source[i] = int(255 * color[i]);
+    }
+    color[0] += (color[0]*alpha_r);
+    color[1] += (color[1]*alpha_g);
+    color[2] += (color[2]*alpha_b);
+    ivec3 int_color;
+    for(int i = 0; i < 3; ++i) {
+        color[i] += timeval * tc[0] * alpha_r;
+        int_color[i] = int(255 * color[i]);
+        int_color[i] = int_color[i]%255;
+        color[i] = float(int_color[i])/255;
+    }
+}
+
