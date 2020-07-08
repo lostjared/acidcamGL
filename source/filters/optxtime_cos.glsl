64ad8841d10e8f800fd7f822d3a74276f7ae1dc7
Jared Bruni <jaredbruni@gmail.com>
2020-07-08 04:29:14 -0700

    added new shader

--- source/filters/optxtime_cos.glsl
+++ source/filters/optxtime_cos.glsl
@@ -25,16 +25,18 @@ void main(void)
     for(int i = 0; i < 3; ++i) {
         source[i] = int(255 * color[i]);
     }
-    color[0] = color[0]*(0.142*vpos[0])+(timeval*optx[0]);
-    color[1] = color[1]*(0.341*vpos[1])+(timeval*optx[1]);
-    color[2] = color[2]*(0.451*vpos[2])+(timeval*optx[2]);
+
+    color[0] = color[0] + (cos(optx[0])*timeval);
+    color[1] = color[1] + (cos(optx[1])*timeval);
+    color[2] = color[2] + (cos(optx[2])*timeval);
+    
     ivec3 int_color;
     for(int i = 0; i < 3; ++i) {
         int_color[i] = int(255 * color[i]);
-        int_color[i] = int_color[i]^source[i];
         if(int_color[i] > 255)
             int_color[i] = int_color[i]%255;
         color[i] = float(int_color[i])/255;
     }
 }
+
 
