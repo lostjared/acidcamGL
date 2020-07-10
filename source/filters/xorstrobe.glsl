975bc0219677962147ec9f6c162d7c2661a36b8a
Jared Bruni <jaredbruni@gmail.com>
2020-07-10 00:18:28 -0700

    added new shader

---
+++ source/filters/xorstrobe.glsl
@@ -0,0 +1,48 @@
+
+#version 410
+in vec2 tc;
+out vec4 color;
+in float alpha_r;
+in float alpha_g;
+in float alpha_b;
+in float current_index;
+in float timeval;
+in float alpha;
+in vec3 vpos;
+in vec4 optx_val;
+uniform vec4 optx;
+uniform float alpha_value;
+
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform sampler2D samp;
+uniform float value_alpha_r, value_alpha_g, value_alpha_b;
+uniform float index_value;
+uniform float time_f;
+
+
+void main(void)
+{
+    color = texture(samp, tc);
+    ivec3 source;
+    for(int i = 0; i < 3; ++i) {
+        source[i] = int(255 * color[i]);
+    }
+    vec3 colorval;
+    colorval[0] = (source[0] * alpha_r)/255;
+    colorval[1] = (source[1] * alpha_g)/255;
+    colorval[2] = (source[2] * alpha_b)/255;
+    color[0] = colorval[0] * alpha_r;
+    color[1] = colorval[1] * alpha_g;
+    color[2] = colorval[2] * alpha_b;
+    ivec3 int_color;
+    for(int i = 0; i < 3; ++i) {
+        int_color[i] = int(255 * color[i]);
+        int_color[i] = int(tc[0])^int_color[i]^source[i];
+        if(int_color[i] > 255)
+            int_color[i] = int_color[i]%255;
+        color[i] = float(int_color[i])/255;
+    }
+}
+
+
