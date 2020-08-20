18251e964fc46b97e09326b8e0ca621c24248c09
Jared Bruni <jaredbruni@gmail.com>
2020-08-20 14:21:10 -0700

    new shader

---
+++ filters/flash_gradient_strobe.glsl
@@ -0,0 +1,40 @@
+#version 330
+in vec2 tc;
+out vec4 color;
+in float alpha_r;
+in float alpha_g;
+in float alpha_b;
+in float current_index;
+in float timeval;
+in float alpha;
+in vec3 vpos;
+in vec4 optx_val;
+uniform vec4 optx;
+in vec4 random_value;
+uniform vec4 random_var;
+uniform float alpha_value;
+
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform sampler2D samp;
+uniform float value_alpha_r, value_alpha_g, value_alpha_b;
+uniform float index_value;
+uniform float time_f;
+uniform vec2 iResolution;
+
+uniform float restore_black;
+in float restore_black_value;
+in vec2 iResolution_;
+
+void main(void)
+{
+    if(restore_black_value == 1.0 && texture(samp, tc) == vec4(0, 0, 0, 1))
+        discard;
+    color = texture(samp, tc);
+    vec2 pos = gl_FragCoord.xy / iResolution_.xy;
+    vec4 l = vec4(alpha_r, alpha_g, alpha_b,1.0);
+    color = vec4(pos, 1, 1) * l * color * alpha;
+}
+
+
+
