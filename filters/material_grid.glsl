ef5b209ab7e38aab4d2bb3fe8b8603c36ff56e58
Jared Bruni <jaredbruni@gmail.com>
2020-08-21 06:23:08 -0700

    new shader

--- filters/material_grid.glsl
+++ filters/material_grid.glsl
@@ -13,15 +13,14 @@ uniform vec4 optx;
 in vec4 random_value;
 uniform vec4 random_var;
 uniform float alpha_value;
-
 uniform mat4 mv_matrix;
 uniform mat4 proj_matrix;
 uniform sampler2D samp;
+uniform sampler2D mat_samp;
+
 uniform float value_alpha_r, value_alpha_g, value_alpha_b;
 uniform float index_value;
 uniform float time_f;
-
-
 
 uniform float restore_black;
 in float restore_black_value;
@@ -31,25 +30,14 @@ void main(void)
     if(restore_black_value == 1.0 && texture(samp, tc) == vec4(0, 0, 0, 1))
         discard;
     color = texture(samp, tc);
-    ivec3 source;
-    for(int i = 0; i < 3; ++i) {
-        source[i] = int(255 * color[i]);
-    }
+    vec4 color2 = texture(mat_samp, tc);
+    //vec4 value = vec4(0.2, 0.1, 0.8, 1.0);
+    vec2 color_value = gl_FragCoord.xy / 16;
+    float value1 = sin(color_value.x);
+    float value2 = cos(color_value.y);
     
-    color[0] += tc[0];
-    color[1] += tc[1];
-    color[2] += tc[0]+tc[1];
-    
-    color[0] += 0.1*timeval;
-    color[1] += 0.3*timeval;
-    color[2] += 0.7*timeval;
-    
-    ivec3 int_color;
-    for(int i = 0; i < 3; ++i) {
-        int_color[i] = int(255 * color[i]);
-        int_color[i] = int_color[i]^source[i];
-        if(int_color[i] > 255)
-            int_color[i] = int_color[i]%255;
-        color[i] = float(int_color[i])/255;
-    }
+    color = color * color2 * value1 * value2;
 }
+
+
+
