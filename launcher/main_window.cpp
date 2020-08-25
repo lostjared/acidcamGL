7ba8aaf55775e4cb8971faf8abaa6ca0946c3761
Jared Bruni <jaredbruni@gmail.com>
2020-08-25 04:49:05 -0700

    working in progress

--- launcher/main_window.cpp
+++ launcher/main_window.cpp
@@ -2,5 +2,12 @@
 #include"main_window.hpp"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
-     setFixedSize(1280, 720);
+     setFixedSize(1280, 1280);
+    setWindowTitle("acidcamGL - Start New Session");
+    command_stdout = new QTextEdit("", this);
+    command_stdout->setGeometry(5, 1280/2, 1280-10, 1280/2-5);
+    command = new QLineEdit("", this);
+    command->setGeometry(5, 10, 1280-10, 30);
+    command_stdout->setReadOnly(true);
+    command->setText("acidcamGL ");
  }
--- launcher/main_window.hpp
+++ launcher/main_window.hpp
@@ -3,7 +3,8 @@
 #define __MAIN_WINDOW_H_
 
 #include<QMainWindow>
-
+#include<QTextEdit>
+#include<QLineEdit>
 
 class MainWindow : public QMainWindow {
 
@@ -11,6 +12,10 @@ private:
     Q_OBJECT
 public:
     MainWindow(QWidget *parent = 0);
+    
+private:
+    QTextEdit *command_stdout;
+    QLineEdit *command;
 };
 
 
