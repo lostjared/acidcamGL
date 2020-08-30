b496658cd4159bed06aa5b384aaa5f503d4b2ecf
Jared Bruni <jaredbruni@gmail.com>
2020-08-29 22:50:27 -0700

    updated build script

--- source/macos-static/copy.sh
+++ source/macos-static/copy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-make clean && make -j16 && cp ./acidcamGL /Applications/acidcamGL/acidcamGL.app/Contents/MacOS/acidcamGL && open /Applications/acidcamGL/acidcamGL.app --args -g -p /Applications/acidcamGL/filters
-install_name_tool -change @loader_path/../Frameworks/Syphon.framework/Versions/A/Syphon  @executable_path/../Frameworks/Syphon.framework/Versions/A/Syphon  /Applications/acidcamGL/acidcamGL.app/Contents/MacOS/acidcamGL
+make clean && make -j16 && cp ./acidcamGL /Applications/acidcamGL/acidcamGL.app/Contents/MacOS/acidcamGL 
+install_name_tool -change @loader_path/../Frameworks/Syphon.framework/Versions/A/Syphon  @executable_path/../Frameworks/Syphon.framework/Versions/A/Syphon  /Applications/acidcamGL/acidcamGL.app/Contents/MacOS/acidcamGL && open /Applications/acidcamGL/acidcamGL.app --args -g -p /Applications/acidcamGL/filters
