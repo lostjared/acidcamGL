27c1a67916a11cf94e0eb5344aab7fe95105a2ba
Jared Bruni <jaredbruni@gmail.com>
2020-09-02 03:52:43 -0700

    add symlinks

--- source/macos-static/copy.sh
+++ source/macos-static/copy.sh
@@ -2,3 +2,4 @@
 
 make clean && make -j16 && cp ./acidcamGL /Applications/acidcamGL/acidcamGL.app/Contents/MacOS/acidcamGL 
 install_name_tool -change @loader_path/../Frameworks/Syphon.framework/Versions/A/Syphon  @executable_path/../Frameworks/Syphon.framework/Versions/A/Syphon  /Applications/acidcamGL/acidcamGL.app/Contents/MacOS/acidcamGL && open /Applications/acidcamGL/acidcamGL.app --args -g -p /Applications/acidcamGL/filters
+cp acidcamGL /usr/local/bin
