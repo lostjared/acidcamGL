e8622369117019d1e7bbff60f99e9dba46958a79
Jared Bruni <jaredbruni@gmail.com>
2020-09-06 02:32:19 -0700

    plugin

--- source/macos-static/plugin/plug1/test1.cpp
+++ source/macos-static/plugin/plug1/test1.cpp
@@ -1,5 +1,10 @@
 #include<acidcam/ac.h>
 
 extern "C" void filter(cv::Mat &frame) {
-	std::cout << "here...\n";
+	for(int z = 0; z < frame.rows; ++z) {
+		for(int i = 0; i < frame.cols; ++i) {
+			cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+			pixel = cv::Vec3b(rand()%255, rand()%255, rand()%255);
+		}
+	}
 }
--- source/macos-static/plugin/test-plugin/test-plugin.cpp
+++ source/macos-static/plugin/test-plugin/test-plugin.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include"plugin-program.hpp"
-
+#include<unistd.h>
 
 int main(int argc, char **argv) {
     if(argc == 2) {
@@ -9,9 +9,13 @@ int main(int argc, char **argv) {
             std::cout << "plugin: loaded..\n";
         }
         cv::Mat m = cv::Mat::zeros(480, 640, CV_8UC3);
+        cv::namedWindow("test");
         for(int i = 0; i < 5; ++i) {
             program.exec(m);
+            cv::imshow("test", m);
         }
+        cv::waitKey(1000);
+        sleep(4);
         std::cout << "plugin: tests passed...\n";
         exit(0);
     }
