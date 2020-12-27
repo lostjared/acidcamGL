f9cff2d6a03c484cd4ec233ad4d621affcbb9607
Jared Bruni <jaredbruni@gmail.com>
2020-12-27 12:11:28 -0800

    shader

---
+++ filters/material_reverse_xor.glsl
@@ -0,0 +1,54 @@
+#version 330
+in vec2 tc;
+out vec4 color;
+in float alpha_r;
+in float alpha_g;
+in float alpha_b;
+in float current_index;
+in float timeval;
+in float alpha;
+in vec3 vpos;
+in vec4 optx_val;
+uniform vec4 optx;
+in vec4 random_value;
+uniform vec4 random_var;
+uniform float alpha_value;
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform sampler2D samp;
+uniform sampler2D mat_samp;
+uniform float value_alpha_r, value_alpha_g, value_alpha_b;
+uniform float index_value;
+uniform float time_f;
+uniform vec2 iResolution;
+
+uniform float restore_black;
+in float restore_black_value;
+in vec2 iResolution_;
+uniform vec4 inc_valuex;
+uniform vec4 inc_value;
+
+float random (vec2 st) {
+    return fract(sin(dot(st.xy,
+                         vec2(12.9898,78.233)))*
+                 43758.5453123);
+}
+
+void main(void)
+{
+    if(restore_black_value == 1.0 && texture(samp, tc) == vec4(0, 0, 0, 1))
+        discard;
+    color = texture(samp, tc);
+    vec4 m_color = texture(mat_samp, tc);
+    color = color * m_color;
+    ivec4 source =ivec4(color * 255);
+    vec4 c = vec4(0.2, 0.5, 0.8, 1); // random_value/255;
+    vec4 cval;
+    color += 0.03;
+    ivec3 int_color;
+    for(int i = 0; i < 3; ++i) {
+        int_color[i] = int(255 * color[i]);
+        int_color[i] = int_color[i]^source[3-i-1];
+        color[i] = float(int_color[i])/255;
+    }
+}
