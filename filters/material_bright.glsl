e4d7392e6716de687690d0ffbe6db6e5b471f765
Jared Bruni <jaredbruni@gmail.com>
2021-01-04 18:23:16 -0800

    shader

--- filters/material_bright.glsl
+++ filters/material_bright.glsl
@@ -16,6 +16,7 @@ uniform float alpha_value;
 uniform mat4 mv_matrix;
 uniform mat4 proj_matrix;
 uniform sampler2D samp;
+uniform sampler2D mat_samp;
 uniform float value_alpha_r, value_alpha_g, value_alpha_b;
 uniform float index_value;
 uniform float time_f;
@@ -27,18 +28,17 @@ void main(void)
 {
     if(restore_black_value == 1.0 && texture(samp, tc) == vec4(0, 0, 0, 1))
         discard;
-    color = texture(samp, tc);
+    color = texture(samp, tc) * texture(mat_samp, tc);
     float x = gl_FragCoord.x;
     float y = gl_FragCoord.y;
     vec2 cord1 = vec2(tc[0]/x, tc[1]/y);
-    vec2 cord2 = vec2(tc[0]/x, tc[1]/y);
-    vec2 cord3 = vec2(tc[0]/x, tc[1]/y);
-    vec4 col1 = texture(samp, cord1);
-    vec4 col2 = texture(samp, cord2);
-    vec4 col3 = texture(samp, cord3);
-    color[0] = color[0]+col1[0];
-    color[1] = color[1]+col2[1];
-    color[2] = color[2]+col3[2];
+    vec2 cord2 = vec2(tc[0]/x+32, tc[1]/y+16);
+    vec2 cord3 = vec2(tc[0]/x+64, tc[1]/y+32);
+    vec4 col1 = texture(samp, cord1) * texture(mat_samp, cord1);
+    vec4 col2 = texture(samp, cord2) * texture(mat_samp, cord2);
+    vec4 col3 = texture(samp, cord3) * texture(mat_samp, cord3);
+    color[0] = color[0] + col1[0];
+    color[1] = color[1] + col2[1];
+    color[2] = color[2] + col3[2];
+    color = color * alpha;
 }
-
-
