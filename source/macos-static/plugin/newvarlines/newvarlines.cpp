137045c099b5e7c2c1fe52d1458c5378c1caa3a9
Jared Bruni <jaredbruni@gmail.com>
2021-03-16 01:35:45 -0700

    new var lines plugin

---
+++ source/macos-static/plugin/newvarlines/newvarlines.cpp
@@ -0,0 +1,27 @@
+#include"ac.h"
+
+
+extern "C" void filter(cv::Mat  &frame) {
+    static constexpr int MAX = 32;
+    static ac::MatrixCollection<MAX> collection;
+    collection.shiftFrames(frame);
+    int off = 0;
+    int shift = rand()%MAX;
+    int m = 0;
+    for(int z = 0; z < frame.rows; ++z) {
+        ++m;
+        if(m > shift) {
+            ++off;
+            m = 0;
+            shift = rand()%MAX;
+            if(off > MAX)
+                off = 0;
+        }
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            cv::Mat &m = collection.frames[off];
+            cv::Vec3b &pix = m.at<cv::Vec3b>(z, i);
+            pixel = pix;
+        }
+    }
+}
