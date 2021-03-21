8e89102215e84eea50e7a4cd4f36517c556edb96
Jared Bruni <jaredbruni@gmail.com>
2021-03-21 00:49:39 -0700

    updated test-plugin program

--- source/plugin/test-plugin/test-plugin.cpp
+++ source/plugin/test-plugin/test-plugin.cpp
@@ -3,16 +3,22 @@
 #include<unistd.h>
 
 int main(int argc, char **argv) {
+    
+    if(argc != 2) {
+        std::cerr << "Error: requires one arguemnt of plugin to test...\n";
+        exit(EXIT_FAILURE);
+    }
+    
     if(argc == 2) {
         acidcam::AC_Program program;
         if(program.load(argv[1])) {
             std::cout << "plugin: loaded..\n";
         }
         cv::Mat m = cv::Mat::zeros(480, 640, CV_8UC3);
-        cv::namedWindow("test");
+        cv::namedWindow("test-plugin");
         for(int i = 0; i < 5; ++i) {
             program.exec(m);
-            cv::imshow("test", m);
+            cv::imshow("test-plugin", m);
         }
         cv::waitKey(1000);
         sleep(4);
