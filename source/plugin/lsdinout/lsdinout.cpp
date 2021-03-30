bf423a4d835717eca36dff4ed2fe12c61009e225
Jared Bruni <jaredbruni@gmail.com>
2021-03-30 01:34:59 -0700

    added plugin

---
+++ source/plugin/lsdinout/lsdinout.cpp
@@ -0,0 +1,62 @@
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    static float rgb[3] = { 1,2,3 };
+    static float inc[3] = { 0.1, 0.2, 0.3 };
+    static float max = 7.0;
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            for(int j = 0; j < 3; ++j) {
+                pixel[j] = static_cast<unsigned char>((0.5 * pixel[j]) + (0.5 * pixel[j] * rgb[j]));
+            }
+        }
+    }
+   
+    static int dir = 1;
+    static int dir2 = 1;
+    static int dir3 = 1;
+    
+    if(dir == 1) {
+        rgb[0] += inc[0];
+        rgb[1] += inc[1];
+        rgb[2] += inc[2];
+        if(rgb[0] > max) {
+            dir = 0;
+            if(dir2 == 1) {
+                max += 0.5f;
+                if(max > 15)
+                    dir2 = 0;
+            } else {
+                max -= 0.5f;
+                if(max <= 7.0)
+                   dir2 = 1;
+                   
+            }
+            inc[0] += 0.1;
+            inc[1] += 0.2;
+            inc[2] += 0.3;
+        }
+    } else {
+        rgb[0] -= inc[0];
+        rgb[1] -= inc[1];
+        rgb[2] -= inc[2];
+    
+        if(rgb[0] <= 1.0) {
+            dir = 1;
+            if(inc[0] > 3.0) {
+                if(dir3 == 1) {
+                    inc[0] = -inc[0];
+                    inc[1] = -inc[1];
+                    inc[2] = -inc[2];
+                    dir3 = 0;
+                } else {
+                    inc[0] = 0.1;
+                    inc[1] = 0.2;
+                    inc[2] = 0.3;
+                    dir3 = 1;
+                }
+            }
+        }
+    }
+}
