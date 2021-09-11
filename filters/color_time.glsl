4a4bc1c0344b4ac2803a3f4888df4e1b254f97a5
Jared Bruni <jaredbruni@gmail.com>
2021-09-11 13:34:11 -0700

    update

---
+++ filters/color_time.glsl
@@ -0,0 +1,39 @@
+#version 330
+in vec2 tc;
+out vec4 color;
+in float alpha_r;
+in float alpha_g;
+in float alpha_b;
+in float current_index;
+in float timeval;
+in float alpha;
+in vec3 vpos;
+in vec4 optx_val;
+uniform vec4 optx;
+in vec4 random_value;
+uniform vec4 random_var;
+uniform float alpha_value;
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform sampler2D samp;
+uniform float value_alpha_r, value_alpha_g, value_alpha_b;
+uniform float index_value;
+uniform float time_f;
+
+uniform float restore_black;
+in float restore_black_value;
+
+void main(void)
+{
+    if(restore_black_value == 1.0 && texture(samp, tc) == vec4(0, 0, 0, 1))
+        discard;
+    
+    color = texture(samp, tc);
+    color[0] = 1.5 * color[0] * fract(alpha_r*timeval);
+    color[1] = 1.5 * color[1] * fract(alpha_g*timeval);
+    color[2] = 1.5 * color[2] * fract(alpha_b*timeval);
+    
+}
+
+
+
