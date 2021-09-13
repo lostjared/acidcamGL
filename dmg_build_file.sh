ac67b10066d295cfd203744605222a687d38a7f3
Jared Bruni <jaredbruni@gmail.com>
2021-09-13 05:54:05 -0700

    updated

--- dmg_build_file.sh
+++ dmg_build_file.sh
@@ -1,11 +1,9 @@
-mkdir -p dmg_build/acidcamGL/acidcamGL
-mkdir -p dmg_build/acidcamGL/acidcamGL/filters
-mkdir -p dmg_build/acidcamGL/acidcamGL/playlist
-mkdir -p dmg_build/acidcamGL/acidcamGL/shortcut
-mkdir -p dmg_build/acidcamGL/acidcamGL/custom
-cp -rfv filters dmg_build/acidcamGL/acidcamGL/filters
-cp -rfv playlist dmg_build/acidcamGL/acidcamGL/playlist
-cp -rfv shortcut dmg_buildshortcut/acidcamGL/acidcamGL/shortcut
-cp -rfv custom dmg_build/acidcamGL/acidcamGL/custom
+mkdir -p dmg_build/acidcamGL/acidcamGL/
+
+cp -rfv filters dmg_build/acidcamGL/acidcamGL/
+cp -rfv autofilter dmg_build/acidcamGL/acidcamGL/
+cp -rfv playlist dmg_build/acidcamGL/acidcamGL/
+#cp -rfv shortcut dmg_buildshortcut/acidcamGL/acidcamGL/shortcut
+cp -rfv custom dmg_build/acidcamGL/acidcamGL/
 cp -rfv /Applications/acidcamGL/acidcamGL.app dmg_build/acidcamGL/acidcamGL/
 hdiutil create -volname acidcamGL -srcfolder dmg_build/acidcamGL -ov -format UDZO acidcamGL.dmg
