3c644faa7c9f7222973870d7822aa0a32adf624e
Jared Bruni <jaredbruni@gmail.com>
2021-09-23 20:45:45 -0700

    new plugin

--- source/plugin/frame_stutter/stutter.cpp
+++ source/plugin/frame_stutter/stutter.cpp
@@ -1,8 +1,29 @@
 #include"ac.h"
+#include<cstdlib>
+#include<ctime>
 
 extern "C" void filter(cv::Mat  &frame) {
-    static constexpr int MAX = 16;
-    static ac::MatrixCollection<MAX> collection;
-    collection.shiftFrames(frame);
+    static cv::Mat stored;
+    static cv::Size stored_size;
+    
+    if(stored_size != frame.size()) {
+        srand(static_cast<int>(time(0)));
+        stored = frame.clone();
+        stored_size = frame.size();
+    } else {
+        if(stored.empty())
+            stored = frame.clone();
 
+        static bool on = true;
+        if(on == true) {
+            if((rand()%25)==0) {
+                stored = frame.clone();
+                on = !on;
+            }
+            frame = stored.clone();
+        } else {
+            if((rand()%30) == 0)
+                on = !on;
+        }
+    }
 }
