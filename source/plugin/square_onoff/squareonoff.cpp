a11b7714601eec165679368ccd225aede7a75652
Jared Bruni <jaredbruni@gmail.com>
2021-10-22 05:24:18 -0700

    added code to check for bounds

--- source/plugin/square_onoff/squareonoff.cpp
+++ source/plugin/square_onoff/squareonoff.cpp
@@ -17,8 +17,8 @@ extern "C" void filter(cv::Mat  &frame) {
     for(int z = 0; z < frame.rows; z += PIXEL_SIZE) {
         for(int i = 0; i < frame.cols; i += PIXEL_SIZE) {
             if((rand()%2)==0) {
-                for(int y = z; y < z+PIXEL_SIZE; ++y) {
-                    for(int x = i; x < i+PIXEL_SIZE; ++x) {
+                for(int y = z; y < z+PIXEL_SIZE && y < frame.rows; ++y) {
+                    for(int x = i; x < i+PIXEL_SIZE && x < frame.cols; ++x) {
                         cv::Vec3b &pixel = frame.at<cv::Vec3b>(y, x);
                         cv::Mat &m = collection.frames[off];
                         cv::Vec3b &pix_off = m.at<cv::Vec3b>(y, x);
@@ -28,7 +28,6 @@ extern "C" void filter(cv::Mat  &frame) {
                 }
             }
         }
-        
         ++off;
         if(off > MAX-1)
             off = 0;
