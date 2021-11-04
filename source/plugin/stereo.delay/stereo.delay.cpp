26e0f6e54d993f0126383328814db2241486948b
Jared Bruni <jaredbruni@gmail.com>
2021-11-04 15:29:16 -0700

    new plugin

---
+++ source/plugin/stereo.delay/stereo.delay.cpp
@@ -0,0 +1,28 @@
+#include"ac.h"
+#include<cstdlib>
+#include<ctime>
+
+extern "C" void filter(cv::Mat  &frame) {
+    cv::Mat img;
+    ac::ac_resize(frame, img, cv::Size(frame.cols/2, frame.rows));
+    static ac::MatrixCollection<8> collection;
+    collection.shiftFrames(img);
+    cv::Mat &copy = collection.frames[7];
+    for(int z = 0; z < frame.rows; ++z) {
+        int x = 0;
+        for(int i = frame.cols/2+10; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            cv::Vec3b pix =img.at<cv::Vec3b>(z, x);
+            ++x;
+            pixel = pix;
+        }
+        for(int i = 0; i < frame.cols/2; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            cv::Vec3b pix =copy.at<cv::Vec3b>(z, i);
+            pixel = pix;
+        }
+        for(int i = frame.cols/2; i < frame.cols/2+10; ++i) {
+            frame.at<cv::Vec3b>(z, i) = cv::Vec3b(0, 0, 0);
+        }
+    }
+}
