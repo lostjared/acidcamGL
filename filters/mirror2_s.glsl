c72fbc25656c5afb20b19c29d32795fcfd1c07cd
Jared Bruni <jaredbruni@gmail.com>
2022-01-03 19:39:52 -0800

    new shaders

---
+++ filters/mirror2_s.glsl
@@ -0,0 +1,42 @@
+#version 330
+in vec2 tc;
+out vec4 color;
+in float alpha_r;
+in float alpha_g;
+in float alpha_b;
+in float current_index;
+in float timeval;
+in float alpha;
+in vec3 vpos;
+in vec4 optx_val;
+uniform vec4 optx;
+in vec4 random_value;
+uniform vec4 random_var;
+uniform float alpha_value;
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform sampler2D samp;
+uniform float value_alpha_r, value_alpha_g, value_alpha_b;
+uniform float index_value;
+uniform float time_f;
+
+uniform float restore_black;
+in float restore_black_value;
+in vec2 iResolution_;
+
+void main(void)
+{
+    if(restore_black_value == 1.0 && texture(samp, tc) == vec4(0, 0, 0, 1))
+        discard;
+    color = texture(samp, tc);
+    ivec4 source = ivec4(255 * color);
+    vec2 pos = gl_FragCoord.xy/iResolution_.xy;
+    vec2 vpos;
+    vpos[0] = 1.0-tc[0];
+    vpos[1] = tc[1];
+    vec4 color2 = texture(samp, vpos);
+    color = (0.8 * color) + (0.8 * color2);
+}
+
+
+
