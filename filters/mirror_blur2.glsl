b8124bb1208d4b173a56aaef09d194ca204574e2
Jared Bruni <jaredbruni@gmail.com>
2022-01-09 13:31:09 -0800

    blur

---
+++ filters/mirror_blur2.glsl
@@ -0,0 +1,46 @@
+#version 330
+in vec2 tc;
+out vec4 color;
+in float alpha_r;
+in float alpha_g;
+in float alpha_b;
+in float current_index;
+in float timeval;
+in float alpha;
+in vec3 vpos;
+in vec4 optx_val;
+uniform vec4 optx;
+in vec4 random_value;
+uniform vec4 random_var;
+uniform float alpha_value;
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform sampler2D samp;
+uniform float value_alpha_r, value_alpha_g, value_alpha_b;
+uniform float index_value;
+uniform float time_f;
+
+uniform float restore_black;
+in float restore_black_value;
+in vec2 iResolution_;
+
+void main(void)
+{
+    if(restore_black_value == 1.0 && texture(samp, tc) == vec4(0, 0, 0, 1))
+        discard;
+    color = texture(samp, tc);
+    ivec4 source = ivec4(255 * color);
+    vec2 pos = gl_FragCoord.xy/iResolution_.xy;
+    vec2 vpos;
+    vpos[0] = 1.0-tc[0];
+    vpos[1] = tc[1];
+    vec4 color2 = texture(samp, vpos);
+    vec4 color3 = texture(samp, tc-0.01);
+    vec4 color4 = texture(samp, tc+0.01);
+    color = (0.3 * color) + (0.3 * color2) + (0.3 * color3) + (0.3 * color4);
+}
+
+
+
+
+
