343230cff0e083fd97216247bbf71f7f6157b1f3
Jared Bruni <jaredbruni@gmail.com>
2022-01-09 14:55:33 -0800

    added script to sort out a list of outputted images to use with img2mov to make slide show

---
+++ scripts/sort_images.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+my $dir = shift @ARGV;
+opendir $d, $dir or die("Could not open directory");
+my @f = readdir $d;
+closedir $d;
+my %list = ();
+foreach $fi (@f) {
+    my $pos = index $fi, "-";
+    if($pos != -1) {
+        my $pos2 = index $fi, "_", $pos;
+        my $pos3 = index $fi, "_", $pos2+1;
+        if($pos3 != -1) {
+            my $r = substr($fi, $pos+1, $pos3-$pos-1);
+            $list{$r} = $fi;
+            #print $r . "\n";
+        }
+    }
+}
+foreach $i (sort keys %list) {
+    print $list{$i} . "\n";
+}
