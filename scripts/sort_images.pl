637a93d9b8c00959648a000c2efce3bdccbc2022
Jared Bruni <jaredbruni@gmail.com>
2022-01-10 14:34:54 -0800

    updated script

--- scripts/sort_images.pl
+++ scripts/sort_images.pl
@@ -1,17 +1,19 @@
 #!/usr/bin/perl
 my $dir = shift @ARGV;
+my $src = shift @ARGV;
 opendir $d, $dir or die("Could not open directory");
 my @f = readdir $d;
 closedir $d;
 my %list = ();
 foreach $fi (@f) {
-    my $pos = index $fi, "-";
+    my $pos = index $fi, $src;
     if($pos != -1) {
         my $pos2 = index $fi, "_", $pos;
         if($pos2 != -1) {
             my $pos3 = index $fi, "_", $pos2+1;
             if($pos3 != -1) {
-                my $r = substr($fi, $pos+1, $pos3-$pos-1);
+                my $r = substr($fi, $pos, $pos3-$pos-1);
+                
                 $list{$r} = $fi;
                 #print $r . "\n";
             }
