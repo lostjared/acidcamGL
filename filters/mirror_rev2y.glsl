1860cc0ffab8e0a8610d1da6f717ce22503c4c6b
Jared Bruni <jaredbruni@gmail.com>
2022-01-16 14:21:21 -0800

    mirror shader

---
+++ filters/mirror_rev2y.glsl
@@ -0,0 +1,60 @@
+#version 330
+in vec2 tc;
+out vec4 color;
+in float alpha_r;
+in float alpha_g;
+in float alpha_b;
+in float current_index;
+in float timeval;
+in float alpha;
+in vec3 vpos;
+in vec4 optx_val;
+uniform vec4 optx;
+in vec4 random_value;
+uniform vec4 random_var;
+uniform float alpha_value;
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform sampler2D samp;
+uniform float value_alpha_r, value_alpha_g, value_alpha_b;
+uniform float index_value;
+uniform float time_f;
+
+uniform float restore_black;
+in float restore_black_value;
+in vec2 iResolution_;
+
+vec4 xor_RGB(vec4 icolor, ivec4 isource) {
+    ivec3 int_color;
+    for(int i = 0; i < 3; ++i) {
+        int_color[i] = int(255 * icolor[i]);
+        int_color[i] = int_color[i]^isource[i];
+        if(int_color[i] > 255)
+            int_color[i] = int_color[i]%255;
+        icolor[i] = float(int_color[i])/255;
+    }
+    return icolor;
+}
+
+void main(void)
+{
+    if(restore_black_value == 1.0 && texture(samp, tc) == vec4(0, 0, 0, 1))
+        discard;
+    
+    vec2 vpos;
+    vpos[1] = 1.0-tc[1];
+    vpos[0] = tc[0];
+    
+    vec2 pos = gl_FragCoord.xy / iResolution_.xy;
+    
+    if(pos[1] > 0.5)
+        color = texture(samp, tc);
+    else
+        color = texture(samp, vpos);
+    
+}
+
+
+
+
+
