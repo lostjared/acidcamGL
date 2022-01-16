625fcfe68588ffc92c20b52a69b2d21a1ad5f7cc
Jared Bruni <jaredbruni@gmail.com>
2022-01-15 23:35:36 -0800

    added reverse shader

--- filters/reverse.glsl
+++ filters/reverse.glsl
@@ -13,7 +13,6 @@ uniform vec4 optx;
 in vec4 random_value;
 uniform vec4 random_var;
 uniform float alpha_value;
-
 uniform mat4 mv_matrix;
 uniform mat4 proj_matrix;
 uniform sampler2D samp;
@@ -21,28 +20,19 @@ uniform float value_alpha_r, value_alpha_g, value_alpha_b;
 uniform float index_value;
 uniform float time_f;
 
-uniform vec4 inc_valuex;
-uniform vec4 inc_value;
-
 uniform float restore_black;
 in float restore_black_value;
+in vec2 iResolution_;
 
 void main(void)
 {
     if(restore_black_value == 1.0 && texture(samp, tc) == vec4(0, 0, 0, 1))
         discard;
-    color = texture(samp, tc);
-    ivec4 source = ivec4(color * 255);
-    ivec4 inc = ivec4(inc_valuex);
-    ivec4 inc_v = ivec4(inc_value);
-    
-    source += inc + inc_v;
-    
-    for(int i = 0; i < 3; ++i) {
-        source[i] = source[i]%255;
-        color[i] = float(source[i])/255;
-    }
+    vec2 vpos = tc;
+    vpos[0] = 1.0-tc[0];
+    color = texture(samp, vpos);
 }
+
 
 
 
