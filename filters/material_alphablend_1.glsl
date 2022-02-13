74e5bdeac3bc4e5aa9db72a5af064228bcc44056
Jared Bruni <jaredbruni@gmail.com>
2022-02-13 15:05:25 -0800

    added alphablending shaders

---
+++ filters/material_alphablend_1.glsl
@@ -0,0 +1,44 @@
+#version 330
+in vec2 tc;
+out vec4 color;
+in float alpha_r;
+in float alpha_g;
+in float alpha_b;
+in float current_index;
+in float timeval;
+in float alpha;
+in vec3 vpos;
+in vec4 optx_val;
+uniform vec4 optx;
+in vec4 random_value;
+uniform vec4 random_var;
+uniform float alpha_value;
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform sampler2D samp;
+uniform sampler2D mat_samp;
+
+uniform float value_alpha_r, value_alpha_g, value_alpha_b;
+uniform float index_value;
+uniform float time_f;
+in vec2 iResolution_;
+uniform vec2 iResolution;
+uniform float restore_black;
+in float restore_black_value;
+
+float random (vec2 st) {
+    return fract(sin(dot(st.xy,
+                         vec2(12.9898,78.233)))*
+        43758.5453123);
+}
+
+void main(void)
+{
+    if(restore_black_value == 1.0 && texture(samp, tc) == vec4(0, 0, 0, 1))
+        discard;
+    color = texture(samp, tc);
+    vec4 color2;
+    color2 = texture(mat_samp, tc);
+    color = (0.4 * color) + (0.6 * color2);
+}
+
