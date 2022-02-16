522c7029222889e8b64fc988a698f8013c5eacfc
Jared Bruni <jaredbruni@gmail.com>
2022-02-16 11:56:21 -0800

    plugin

---
+++ source/plugin/frame_offset_rsize_skip/rsize.cpp
@@ -0,0 +1,54 @@
+#include "ac.h"
+
+unsigned char wrap_cast(double d) {
+    unsigned long x = static_cast<unsigned long>(d);
+    if(x > 255)
+        x = x%255;
+    return static_cast<unsigned char>(x);
+}
+
+extern "C" void filter(cv::Mat &frame) {
+    static ac::MatrixCollection<8> collection;
+    if(collection.empty()) {
+        srand(static_cast<unsigned int>(time(0)));
+    }
+    collection.shiftFrames(frame);
+    static int rsize = 150;
+    int rsize_c = 0;
+    int rsize_count = rand()%rsize;
+    static int offset = 0;
+    int rsize_x = rand()%rsize;
+    bool on = true;
+    if( rand()%10 > 5 )
+        on = false;
+    
+    for(int z = 0; z < frame.rows; ++z) {
+        int offset_x = 0;
+        for(int i = rsize_x; i < frame.cols; ++i) {
+            if(on == false) continue;
+            
+            cv::Mat &m = collection.frames[offset];
+            cv::Vec3b &pixel_off = m.at<cv::Vec3b>(z, offset_x);
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            
+            for(int j = 0; j < 3; ++j) {
+                pixel[j] = wrap_cast((pixel[j] * 0.5) + (0.5 * pixel_off[j]));
+            }
+            
+            offset_x++;
+        }
+        
+        ++rsize_c;
+        if(rsize_c > rsize_count) {
+            rsize = 150+rand()%50;
+            rsize_x = rand()%rsize;
+            rsize_c = 0;
+            rsize_count = 125+rand()%rsize;
+            offset++;
+            if(offset > (collection.size()-1)) {
+                offset = 0;
+            }
+            on = (on == true) ? false : true;
+        }
+    }
+}
