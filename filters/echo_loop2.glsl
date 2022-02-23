82f6c8287ee288ce1ee9a66e1d2ce4b4c9cf6d27
Jared Bruni <jaredbruni@gmail.com>
2022-02-23 13:53:35 -0800

    reverse echo loop

---
+++ filters/echo_loop2.glsl
@@ -0,0 +1,49 @@
+#version 330
+in vec2 tc;
+out vec4 color;
+in float alpha_r;
+in float alpha_g;
+in float alpha_b;
+in float current_index;
+in float timeval;
+in float alpha;
+in vec3 vpos;
+in vec4 optx_val;
+uniform vec4 optx;
+in vec4 random_value;
+uniform vec4 random_var;
+uniform float alpha_value;
+
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform sampler2D samp;
+uniform float value_alpha_r, value_alpha_g, value_alpha_b;
+uniform float index_value;
+uniform float time_f;
+
+
+uniform float restore_black;
+in float restore_black_value;
+
+vec4 echo_loop(vec4 color, int count) {
+    vec4 col = color;
+    for(int i = 2; i < count; i += 2) {
+        vec4 pos = texture(samp, (1.0-tc)/i);
+        col = (0.6 * col) + (0.2 * pos);
+    }
+    return col;
+}
+
+void main(void)
+{
+    if(restore_black_value == 1.0 && texture(samp, tc) == vec4(0, 0, 0, 1))
+        discard;
+    color = texture(samp, tc);
+    color = echo_loop(color, 8);
+
+}
+
+
+
+
+
