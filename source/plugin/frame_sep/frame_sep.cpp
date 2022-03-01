821d8d118b9d1cba91b59ad6f58d8b818e572329
Jared Bruni <jaredbruni@gmail.com>
2022-02-28 22:15:48 -0800

    updated

---
+++ source/plugin/frame_sep/frame_sep.cpp
@@ -0,0 +1,18 @@
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    static constexpr int MAX = 16;
+    static ac::MatrixCollection<MAX> collection;
+    collection.shiftFrames(frame);
+    static int off = 0;
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            cv::Mat &f = collection.frames[off];
+            cv::Vec3b pix = f.at<cv::Vec3b>(z, i);
+            pixel = pix;
+        }
+        if(((z%240)==0) && ++off > MAX-1)
+            off = 0;
+    }
+}
