bd67399caf0e2297e81517045b4bf741453f21ed
Jared Bruni <jaredbruni@gmail.com>
2022-02-28 19:22:10 -0800

    new plugin

---
+++ source/plugin/stuckline/stuckline.cpp
@@ -0,0 +1,27 @@
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    static constexpr int MAX = 16;
+    static ac::MatrixCollection<MAX> collection;
+  
+    if(collection.empty()) {
+        srand(static_cast<unsigned int>(time(0)));
+        collection.shiftFrames(frame);
+    }
+    
+    if (rand()%10 == 0)
+        collection.shiftFrames(frame);
+    static int off = 0;
+    static int array_off = 0;
+    
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            cv::Mat &f = collection.frames[off];
+            cv::Vec3b pix = f.at<cv::Vec3b>(z, i);
+            pixel = pix;
+        }
+        if(++off > MAX-1)
+            off = 0;
+    }
+}
