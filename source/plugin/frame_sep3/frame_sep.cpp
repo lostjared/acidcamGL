2510d8c8732f4e64394211a36d4180ff8b5ecea0
Jared Bruni <jaredbruni@gmail.com>
2022-03-04 22:26:37 -0800

    plugin

---
+++ source/plugin/frame_sep3/frame_sep.cpp
@@ -0,0 +1,37 @@
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    static constexpr int MAX = 16;
+    static ac::MatrixCollection<MAX> collection;
+
+    if(collection.empty()) {
+        collection.shiftFrames(frame);
+        srand(static_cast<unsigned int>(time(0)));
+    } else if(rand()%5==0)
+        collection.shiftFrames(frame);
+    static bool on = false;
+    static int off = 0;
+    static int line_wait = 2+rand()%300;
+    
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            if(on == false) break;
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            cv::Mat &f = collection.frames[off];
+            cv::Vec3b pix = f.at<cv::Vec3b>(z, i);
+            pixel = pix;
+        }
+        if(((z%line_wait)==0) && ++off > MAX-1) {
+            off = 0;
+            static int cnt = 0;
+            static int wait = rand()%10;
+            ++cnt;
+            if(cnt > wait) {
+                on = !on;
+                cnt = 0;
+                wait = rand()%10;
+            }
+            line_wait = 2+rand()%300;
+        }
+    }
+}
