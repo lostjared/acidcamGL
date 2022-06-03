35266dd0b270794191c4e4d839dff39fc8b038ca
Jared Bruni <jaredbruni@gmail.com>
2022-06-03 01:08:14 -0700

    updated

--- source/plugin/sep_blocks2/sep_blocks.cpp
+++ source/plugin/sep_blocks2/sep_blocks.cpp
@@ -19,7 +19,7 @@ extern "C" void filter(cv::Mat  &frame) {
             for(int z = rand()%(frame.rows-1); z < rand()%(frame.rows-1); ++z) {
                 for(int i = 0; i < frame.cols; ++i) {
                     cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
-                    pixel = ac::pixelAt(collection.frames[7], z, i);
+                    pixel = ac::pixelAt(collection.frames[offset], z, i);
                     
                 }
             }
