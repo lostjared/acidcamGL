6465f9f41c5ca6f6e08f7d4291d4c17ee836fd43
Jared Bruni <jaredbruni@gmail.com>
2022-06-03 01:30:58 -0700

    plugin

---
+++ source/plugin/sep_blocks3/sep_blocks.cpp
@@ -0,0 +1,31 @@
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    static constexpr int MAX = 16;
+    static ac::MatrixCollection<16> collection;
+    if(collection.empty()) {
+        srand(static_cast<unsigned int>(time(0)));
+        collection.shiftFrames(frame);
+    }
+    else if(rand()%6 == 0)
+        collection.shiftFrames(frame);
+    
+    static bool strobe = false;
+    static int offset = 0;
+    
+    if(strobe) {
+        for(int j = 0; j < 25+rand()%25; ++j) {
+            offset = rand()%(MAX-1);
+            for(int z = rand()%(frame.rows-1); z < 50 +(rand()%(frame.rows-1)-50); ++z) {
+                for(int i = 0; i < frame.cols; ++i) {
+                    cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+                    pixel = ac::pixelAt(collection.frames[offset], z, i);
+                    
+                }
+            }
+        }
+        
+    }
+    
+    strobe = (strobe == true) ? false : true;
+}
