584e7527d26dd73d3c9b3493ae15d7fd9f6334db
Jared Bruni <jaredbruni@gmail.com>
2022-06-14 00:06:42 -0700

    tri

--- source/plugin/alphablend_tri/tri.cpp
+++ source/plugin/alphablend_tri/tri.cpp
@@ -10,6 +10,16 @@ extern "C" void filter(cv::Mat  &frame) {
     for(int z = 0; z < frame.rows; ++z) {
         for(int i = 0; i < frame.cols; ++i) {
             cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+            
+            cv::Vec3b pix[3];
+            pix[0] = ac::pixelAt(collection.frames[0], z, i);
+            pix[1] = ac::pixelAt(collection.frames[3], z, i);
+            pix[2] = ac::pixelAt(collection.frames[7], z, i);
+            
+            pixel[0] = (0.33 * pix[0][0]) + (0.33 * pix[1][0]) + (0.33 * pix[2][0]);
+            pixel[1] = (0.33 * pix[0][1]) + (0.33 * pix[1][1]) + (0.33 * pix[2][1]);
+            pixel[2] = (0.33 * pix[0][2]) + (0.33 * pix[1][2]) + (0.33 * pix[2][2]);
+
         }
     }
 }
