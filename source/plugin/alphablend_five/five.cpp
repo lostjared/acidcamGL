4cd2464ad9b6eb559eb8ae34ee8039dba81473c1
Jared Bruni <jaredbruni@gmail.com>
2022-06-15 01:48:36 -0700

    updated

--- source/plugin/alphablend_five/five.cpp
+++ source/plugin/alphablend_five/five.cpp
@@ -11,14 +11,17 @@ extern "C" void filter(cv::Mat  &frame) {
         for(int i = 0; i < frame.cols; ++i) {
             cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
             
-            cv::Vec3b pix[3];
+            cv::Vec3b pix[5];
             pix[0] = ac::pixelAt(collection.frames[0], z, i);
-            pix[1] = ac::pixelAt(collection.frames[3], z, i);
-            pix[2] = ac::pixelAt(collection.frames[7], z, i);
-            
-            pixel[0] = (0.33 * pix[0][0]) + (0.33 * pix[1][0]) + (0.33 * pix[2][0]);
-            pixel[1] = (0.33 * pix[0][1]) + (0.33 * pix[1][1]) + (0.33 * pix[2][1]);
-            pixel[2] = (0.33 * pix[0][2]) + (0.33 * pix[1][2]) + (0.33 * pix[2][2]);
+            pix[1] = ac::pixelAt(collection.frames[1], z, i);
+            pix[2] = ac::pixelAt(collection.frames[2], z, i);
+            pix[3] = ac::pixelAt(collection.frames[3], z, i);
+            pix[4] = ac::pixelAt(collection.frames[4], z, i);
+
+
+            pixel[0] = (0.2 * pix[0][0]) + (0.2 * pix[1][0]) + (0.2 * pix[2][0]) + (0.2 * pix[3][0]) + (0.2 * pix[4][0]);
+            pixel[1] = (0.2 * pix[0][1]) + (0.2 * pix[1][1]) + (0.2 * pix[2][1]) + (0.2 * pix[3][1]) + (0.2 * pix[4][1]);;
+            pixel[2] = (0.2 * pix[0][2]) + (0.2 * pix[1][2]) + (0.2 * pix[2][2]) + (0.2 * pix[3][2]) + (0.2 * pix[4][2]);;
 
         }
     }
