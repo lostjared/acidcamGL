f667da71883ef248a29bc40439246306efacd5f1
Jared Bruni <jaredbruni@gmail.com>
2022-06-18 00:46:39 -0700

    inital new plugin

---
+++ source/plugin/alpha_diamond2/alpha.cpp
@@ -0,0 +1,52 @@
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    static constexpr int MAX = 8;
+    static ac::MatrixCollection<MAX> collection;
+    if(collection.empty())
+        collection.shiftFrames(frame);
+    
+    collection.shiftFrames(frame);
+    
+    static int index = 0;
+    static int offset = 0;
+    int start_x = 0;
+    int off_x = index;
+    for(int z = 0; z < frame.rows; ++z) {
+        start_x = off_x;
+        for(int i = 0; i < frame.cols; ++i) {
+            if(start_x > frame.cols-1) {
+                start_x = 0;
+            }
+            cv::Vec3b &pixel = ac::pixelAt(frame,z, start_x);
+            cv::Vec3b pix = collection.frames[offset].at<cv::Vec3b>(z, i);
+            pixel[0] = ac::wrap_cast((0.5 * pixel[0]) + (0.5 * pix[0]));
+            pixel[1] = ac::wrap_cast((0.5 * pixel[1]) + (0.5 * pix[1]));
+            pixel[2] = ac::wrap_cast((0.5 * pixel[2]) + (0.5 * pix[2]));
+//            pixel = pix;
+            ++start_x;
+        }
+        ++off_x;
+        if(off_x > frame.cols)
+            off_x = 0;
+    }
+    ++index;
+    if(index > frame.cols)
+        index = 0;
+    ac::MirrorLeftBottomToTop(frame);
+    
+    static int dir = 1;
+    
+    if(dir == 1) {
+        if(++offset > MAX-1) {
+            offset = MAX-1;
+            dir = 0;
+        }
+    } else {
+        if(--offset <= 1) {
+            offset = 1;
+            dir = 1;
+        }
+    }
+}
+    
