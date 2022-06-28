c1de9a5d2c7287ad53129598359689df1f88ccb4
Jared Bruni <jaredbruni@gmail.com>
2022-06-28 02:53:11 -0700

    plugin

--- source/plugin/frame_offset_draw/offset-draw.cpp
+++ source/plugin/frame_offset_draw/offset-draw.cpp
@@ -7,9 +7,24 @@ extern "C" void filter(cv::Mat  &frame) {
         collection.shiftFrames(frame);
     collection.shiftFrames(frame);
     
+    static char offset = 0xF;
+    
     for(int z = 0; z < frame.rows; ++z) {
         for(int i = 0; i < frame.cols; ++i) {
-            cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            if(z+1 < frame.rows && i+1 < frame.cols) {
+                cv::Vec3b pix[4];
+                pix[0] = collection.frames[1].at<cv::Vec3b>(z+1, i);
+                pix[1] = collection.frames[3].at<cv::Vec3b>(z, i+1);
+                pix[2] = collection.frames[7].at<cv::Vec3b>(z+1, i+1);
+             
+                for(int j = 0; j < 3; ++j)
+                    if(abs(pixel[j]-pix[j][j]) > 25)
+                        pixel[j] += pix[j][j] ^ offset;
+            }
         }
+    }
+    if(++offset > 120) {
+        offset = 0xF;
     }
 }
