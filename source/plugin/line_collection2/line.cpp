c378fc30d3fbd9f11a854181f3ce68fdf49f1139
Jared Bruni <jaredbruni@gmail.com>
2022-07-03 00:02:29 -0700

    plugin

---
+++ source/plugin/line_collection2/line.cpp
@@ -0,0 +1,28 @@
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    static constexpr int MAX = 8;
+    static ac::MatrixCollection<MAX> collection;
+    if(collection.empty())
+        collection.shiftFrames(frame);
+    collection.shiftFrames(frame);
+    
+    
+    for(int z = 0; z < frame.rows; ++z) {
+        
+        int cnt = rand()%(frame.cols/32);
+        int offset = 0;
+      
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+            cv::Vec3b pix = collection.frames[offset].at<cv::Vec3b>(z, i);
+            pixel = pix;
+            
+            if(++cnt > frame.cols/32) {
+                cnt = 0;
+                if(++offset > MAX-1)
+                    offset = 0;
+            }
+        }
+    }
+}
