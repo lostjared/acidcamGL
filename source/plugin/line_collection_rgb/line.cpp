a805753d9ec657d2ddd2a2921301e7431624f97f
Jared Bruni <jaredbruni@gmail.com>
2022-07-06 12:18:09 -0700

    updated

--- source/plugin/line_collection_rgb/line.cpp
+++ source/plugin/line_collection_rgb/line.cpp
@@ -12,6 +12,7 @@ extern "C" void filter(cv::Mat  &frame) {
     
     
     static int r_count = rand()%50;
+    static int rgb = 0;
     
     for(int z = 0; z < frame.rows; ++z) {
         
@@ -21,13 +22,16 @@ extern "C" void filter(cv::Mat  &frame) {
         for(int i = 0; i < frame.cols; ++i) {
             cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
             cv::Vec3b pix = collection.frames[offset].at<cv::Vec3b>(z, i);
-            pixel = pix;
+            pixel[rgb] = pix[rgb];
             
             if(++cnt > r_count) {
                 cnt = 0;
                 r_count = rand()%50;
-                if(++offset > MAX-1)
+                if(++offset > MAX-1) {
                     offset = 0;
+                    if(++rgb > 2)
+                        rgb = 0;
+                }
             }
         }
     }
