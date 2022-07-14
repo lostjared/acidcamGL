d12e269ada868d609fae2888e26a1970ccb37531
Jared Bruni <jaredbruni@gmail.com>
2022-07-14 11:07:51 -0700

    new one

---
+++ source/plugin/opposite_dir/blank.cpp
@@ -0,0 +1,47 @@
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    static constexpr int MAX = 8;
+    static ac::MatrixCollection<MAX> collection;
+    if(collection.empty())
+        collection.shiftFrames(frame);
+    collection.shiftFrames(frame);
+    
+    static int offset = 0;
+    static int cx = rand()%50;
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+            cv::Vec3b pix = collection.frames[offset].at<cv::Vec3b>(z, i);
+            for(int q = 0; q < 3; ++q)
+                pixel[q] = ac::wrap_cast((0.5 * pixel[q]) + (0.5 * pix[q]));
+
+        }
+        static int c = 0;
+        if(++c > cx) {
+            if(++offset >(MAX-1))
+                offset = 0;
+            c = 0;
+            if(++cx > 100)
+                cx = rand()%50;
+        }
+
+    }
+    
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+            cv::Vec3b pix = collection.frames[offset].at<cv::Vec3b>(z, i);
+            for(int q = 0; q < 3; ++q)
+                pixel[q] = ac::wrap_cast((0.5 * pixel[q]) + (0.5 * pix[q]));
+        }
+        static int c = 0;
+        if(++c > cx) {
+            if(--offset <= 0)
+                offset = MAX-1;
+            c = 0;
+            if(++cx > 100)
+                cx = rand()%50;
+        }
+    }
+}
