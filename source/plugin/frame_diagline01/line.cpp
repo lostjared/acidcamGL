89d623294ed6ab0ab237b8a0a8e3db9f47d43f5e
Jared Bruni <jaredbruni@gmail.com>
2022-07-31 14:11:28 -0700

    updated

--- source/plugin/frame_diagline01/line.cpp
+++ source/plugin/frame_diagline01/line.cpp
@@ -3,12 +3,24 @@
 extern "C" void filter(cv::Mat  &frame) {
     static constexpr int MAX = 8;
     static ac::MatrixCollection<MAX> collection;
-    if(collection.empty())
+    if(collection.empty()) {
+        srand(static_cast<unsigned int>(time(0)));
         collection.shiftFrames(frame);
+    }
     collection.shiftFrames(frame);
     
     static int line_start_x = rand()%(frame.cols-1);
     static int line_dip = rand()%50;
+    static int cnt = 0;
+    static int rnd_max = rand()%50;
+    static bool none = false;
+    if(++cnt > rnd_max) {
+        cnt = 0;
+        rnd_max = rand()%50;
+        none = !none;
+    }
+    if(none == true) return;
+    
     for(int z = 0; z < frame.rows; ++z) {
     
         int line_inc = 0;
