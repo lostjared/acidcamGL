1216fb54db1af2f14bc0b340f34cb63b399f4740
Jared Bruni <jaredbruni@gmail.com>
2022-07-31 14:08:47 -0700

    updated

--- source/plugin/frame_diagline01/line.cpp
+++ source/plugin/frame_diagline01/line.cpp
@@ -7,9 +7,30 @@ extern "C" void filter(cv::Mat  &frame) {
         collection.shiftFrames(frame);
     collection.shiftFrames(frame);
     
+    static int line_start_x = rand()%(frame.cols-1);
+    static int line_dip = rand()%50;
     for(int z = 0; z < frame.rows; ++z) {
-        for(int i = 0; i < frame.cols; ++i) {
+    
+        int line_inc = 0;
+        int line_pos = 0;
+        
+        for(int i = line_start_x; i < frame.cols; ++i) {
             cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+            ++line_pos;
+            if(line_pos > line_dip) {
+                ++line_inc;
+            }
+            
+            if(z+line_inc < frame.rows) {
+                cv::Vec3b pix = collection.frames[offset].at<cv::Vec3b>(z+line_inc, i);
+                pixel = pix;
+            }
         }
+    }
+    
+    line_start_x += 100;
+    if(line_start_x > frame.cols) {
+        line_start_x = rand()%(frame.cols-1);
+        line_dip = rand()%50;
     }
 }
