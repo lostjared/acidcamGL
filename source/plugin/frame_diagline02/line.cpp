72a779559477eb9227d7f291366538dd831778f9
Jared Bruni <jaredbruni@gmail.com>
2022-07-31 14:39:18 -0700

    updated

--- source/plugin/frame_diagline02/line.cpp
+++ source/plugin/frame_diagline02/line.cpp
@@ -9,19 +9,13 @@ extern "C" void filter(cv::Mat  &frame) {
     }
     collection.shiftFrames(frame);
     
-    int line_start_x = rand()%(frame.cols-1);
-    int line_dip = rand()%10;
+    static int line_start_x = rand()%(frame.cols-1);
+    static int line_dip = rand()%150;
     static int cnt = 0;
     static int rnd_max = rand()%50;
     static bool none = false;
     static int offset = 0;
-    if(++cnt > rnd_max) {
-        cnt = 0;
-        rnd_max = rand()%50;
-        none = !none;
-    }
-    if(none == true) return;
-    
+
     for(int z = 0; z < frame.rows; ++z) {
     
         int line_inc = 0;
@@ -36,14 +30,18 @@ extern "C" void filter(cv::Mat  &frame) {
             
             if(z+line_inc < frame.rows) {
                 cv::Vec3b pix = collection.frames[offset].at<cv::Vec3b>(z+line_inc, i);
-                for(int q = 0; q < 3; ++q) {
-                    pixel[q] = ac::wrap_cast((0.5 * pixel[q]) + (0.5 * pix[q]));
-                }
+                pixel = pix;
             }
         }
     }
     
     if((++offset > (MAX-1))) {
         offset = rand()%(MAX-1);
+    }
+    
+    line_start_x += 50;
+    if(line_start_x > frame.cols-1) {
+        line_start_x = rand()%50;
+        line_dip = rand()%150;
     }
 }
