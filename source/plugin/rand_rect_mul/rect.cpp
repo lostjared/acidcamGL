728702bee92eb28d5899ce09316d0f0e2398461d
Jared Bruni <jaredbruni@gmail.com>
2022-08-10 14:01:46 -0700

    new plugin

--- source/plugin/rand_rect_mul/rect.cpp
+++ source/plugin/rand_rect_mul/rect.cpp
@@ -8,18 +8,18 @@ extern "C" void filter(cv::Mat  &frame) {
         collection.shiftFrames(frame);
     }
     collection.shiftFrames(frame);
-   
-    int cy_start = rand()%(frame.rows-1);
-    int cy_stop = cy_start+(rand()%frame.rows/6);
-    
-    int offset = rand()%(MAX-1);
-    int off = rand()%3;
-    
-    for(int z = cy_start; z < frame.rows && z < cy_stop; ++z) {
-        for(int i = 0; i < frame.cols; ++i) {
-            cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
-            cv::Vec3b cpix = collection.frames[offset].at<cv::Vec3b>(z, i);
-             pixel = cpix;
+    int loop_counter = rand()%25;
+    for(int i = 0; i < loop_counter; ++i) {
+        int cy_start = rand()%(frame.rows-1);
+        int cy_stop = cy_start+(rand()%frame.rows/6);
+        int offset = rand()%(MAX-1);
+        int off = rand()%3;
+        for(int z = cy_start; z < frame.rows && z < cy_stop; ++z) {
+            for(int i = 0; i < frame.cols; ++i) {
+                cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+                cv::Vec3b cpix = collection.frames[offset].at<cv::Vec3b>(z, i);
+                pixel = cpix;
+            }
         }
     }
 }
