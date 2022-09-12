6570df2a88de67b22ed45720f6b753c44eedd862
Jared Bruni <jaredbruni@gmail.com>
2022-09-12 13:37:16 -0700

    updated

--- source/plugin/resize_stretch_v1c/resize.cpp
+++ source/plugin/resize_stretch_v1c/resize.cpp
@@ -9,11 +9,9 @@ extern "C" void filter(cv::Mat  &frame) {
     } else if(rand()%5 == 0) {
         collection.shiftFrames(frame);
     }
-    
     static int wait = 0;
     static int timeout = 10+rand()%24;
     static int offset = 0;
-    
     auto drawStretchedRect = [&](int y, int height, int nw, int nh) {
             
         for(int z = y; z < y+height; ++z) {
@@ -32,22 +30,21 @@ extern "C" void filter(cv::Mat  &frame) {
         }
         
     };
-    
     int nw = frame.cols+(rand()%(frame.cols/4));
     int nh = frame.rows+(rand()%(frame.rows/4));
-    
     auto drawRects = [&](int y, int &wait, int &timeout) {
-   
         if(++wait > timeout) {
             wait = 0;
-            timeout = 10+rand()%24;
+            timeout = 10+rand()%60;
             drawStretchedRect(y, frame.rows/4, nw, nh);
         }
-        
     };
     int rnd = 1+(rand()%2);
     drawRects(0, wait, timeout);
     static int wt = 0, time2 = 0;
     drawRects((frame.rows/4)*rnd, wt, time2);
     
+    ++offset;
+    if(offset > (MAX-1))
+        offset = 0;
 }
