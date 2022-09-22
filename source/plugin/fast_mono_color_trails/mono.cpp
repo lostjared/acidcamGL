10511f852847131b8d8c82510f3602eada0103f6
Jared Bruni <jaredbruni@gmail.com>
2022-09-22 14:18:47 -0700

    fast mono color trails

--- source/plugin/fast_mono_color_trails/mono.cpp
+++ source/plugin/fast_mono_color_trails/mono.cpp
@@ -1,15 +1,22 @@
 #include"ac.h"
 
 extern "C" void filter(cv::Mat  &frame) {
-    static constexpr int MAX = 8;
-    static ac::MatrixCollection<MAX> collection;
-    if(collection.empty())
-        collection.shiftFrames(frame);
+    static ac::MatrixCollection<16> collection;
     collection.shiftFrames(frame);
-    
-    for(int z = 0; z < frame.rows; ++z) {
-        for(int i = 0; i < frame.cols; ++i) {
-            cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+    cv::Mat *frames[3];
+    frames[0] = &collection.frames[1];
+    frames[1] = &collection.frames[7];
+    frames[2] = &collection.frames[15];
+    auto callback = [&](cv::Mat *frame, int offset, int cols, int size) {
+        for(int z = offset; z <  offset+size; ++z) {
+            for(int i = 0; i < cols; ++i) {
+                cv::Vec3b &pixel = frame->at<cv::Vec3b>(z, i);
+                for(int j = 0; j < 3; ++j) {
+                    cv::Vec3b pix = frames[j]->at<cv::Vec3b>(z, i);
+                    pixel[j] = pix[0];
+                }
+            }
         }
-    }
+    };
+    ac::UseMultipleThreads(frame, ac::getThreadCount(), callback);
 }
