80909a05e34003eee8009e3f12a9094a6e6cf293
Jared Bruni <jaredbruni@gmail.com>
2022-09-29 05:31:20 -0700

    added split

--- source/plugin/frame_split/split.cpp
+++ source/plugin/frame_split/split.cpp
@@ -3,13 +3,33 @@
 extern "C" void filter(cv::Mat  &frame) {
     static constexpr int MAX = 8;
     static ac::MatrixCollection<MAX> collection;
-    if(collection.empty())
+    if(collection.empty()) {
         collection.shiftFrames(frame);
+        srand(static_cast<unsigned int>(time(0)));
+    }
+    
+    
+    static int timeout = 0;
+    static int wait = rand()%15+5;
+    
+    if(++timeout > wait) {
+        timeout = 0;
+        wait = rand()%15+5;
+    } else return;
+    
     collection.shiftFrames(frame);
     
-    for(int z = 0; z < frame.rows; ++z) {
-        for(int i = 0; i < frame.cols; ++i) {
-            cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+    auto drawShift = [&](cv::Mat &frame_copy, int y, int h) {
+        for(int z = y; z < y+h && z < frame.rows; ++z) {
+            for(int i = 0; i < frame.cols; ++i) {
+                cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+                cv::Vec3b &pix = frame_copy.at<cv::Vec3b>(z, i);
+                pixel = pix;
+            }
         }
-    }
+    };
+
+    int row = rand()%(frame.rows);
+    int row_h = rand()%(frame.rows);
+    drawShift(collection.frames[7], row, row_h);
 }
