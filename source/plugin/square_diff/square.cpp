f502ca64403dc3de095d3b94e2416e8758c7a0b2
Jared Bruni <jaredbruni@gmail.com>
2022-09-30 09:43:03 -0700

    diff

--- source/plugin/square_diff/square.cpp
+++ source/plugin/square_diff/square.cpp
@@ -3,13 +3,35 @@
 extern "C" void filter(cv::Mat  &frame) {
     static constexpr int MAX = 8;
     static ac::MatrixCollection<MAX> collection;
-    if(collection.empty())
+    if(collection.empty()) {
         collection.shiftFrames(frame);
-    collection.shiftFrames(frame);
+        srand(static_cast<unsigned int>(time(0)));
+    }
+    else
+        collection.shiftFrames(frame);
     
-    for(int z = 0; z < frame.rows; ++z) {
-        for(int i = 0; i < frame.cols; ++i) {
-            cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+    auto drawSquare = [&](cv::Mat sframe, int x, int y, int w, int h) {
+        for(int i = x; i < x+w && i < frame.cols; ++i) {
+            for(int z = y; z < y+h && z < frame.rows; ++z) {
+                cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+                cv::Vec3b &pix = sframe.at<cv::Vec3b>(z, i);
+                pixel = pix;
+            }
+        }
+    };
+    
+    int square_h = 16;
+    int square_w = 64;
+    static int offset = 0;
+    
+    for(int z = 0; z < frame.rows; z += square_h) {
+        square_w = 1+rand()%32;
+        for(int i = 0; i < frame.cols; i += square_w) {
+            drawSquare(collection.frames[offset], i, z, square_w, square_h);
+        }
+        if(++offset > (MAX-1)) {
+            offset = 0;
         }
     }
+    
 }
