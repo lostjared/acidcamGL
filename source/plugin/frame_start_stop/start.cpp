91305212c4f95115d476eaf75d837de5f10fe602
Jared Bruni <jaredbruni@gmail.com>
2022-09-30 02:41:52 -0700

    start/stop delay

--- source/plugin/frame_start_stop/start.cpp
+++ source/plugin/frame_start_stop/start.cpp
@@ -3,13 +3,33 @@
 extern "C" void filter(cv::Mat  &frame) {
     static constexpr int MAX = 8;
     static ac::MatrixCollection<MAX> collection;
-    if(collection.empty())
+    if(collection.empty()) {
         collection.shiftFrames(frame);
-    collection.shiftFrames(frame);
+        srand(static_cast<unsigned int>(time(0)));
+    }
+    static int wait = 5;
+    static int timeout = 0;
     
-    for(int z = 0; z < frame.rows; ++z) {
-        for(int i = 0; i < frame.cols; ++i) {
-            cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+    static int offset = 0;
+    static int delay = 0;
+    static int delay_wait = 3+rand()%3;
+    
+    if(++timeout > wait) {
+        timeout = 0;
+        if(++delay > delay_wait) {
+            wait = 5+rand()%30;
+            delay = 0;
+            delay_wait = 3+rand()%3;
+        } else {
+            wait = 5+rand()%2;
         }
+        ++offset;
+        if(offset > (MAX-1))
+            offset = 0;
+        frame = collection.frames[offset].clone();
+        
+    } else {
+        collection.shiftFrames(frame);
+        frame = collection.frames[7].clone();
     }
 }
