81dd2956a999518f0852c5de3914f48bcb615284
Jared Bruni <jaredbruni@gmail.com>
2022-09-30 02:52:21 -0700

    rsquare_small

--- source/plugin/rsquare_small/rsquare.cpp
+++ source/plugin/rsquare_small/rsquare.cpp
@@ -1,8 +1,8 @@
 #include"ac.h"
 
 void drawSquare(cv::Mat &frame, int x, int y, int w, int h, cv::Mat &off) {
-    for(int i = x; i < w; ++i) {
-        for(int z = y; z < h; ++z) {
+    for(int i = x; i < x+w && i < frame.cols; ++i) {
+        for(int z = y; z < y+h && z < frame.rows; ++z) {
             cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
             pixel = off.at<cv::Vec3b>(z, i);
         }
@@ -15,14 +15,13 @@ extern "C" void filter(cv::Mat  &frame) {
     if(collection.empty()) {
         srand(static_cast<unsigned int>(time(0)));
         collection.shiftFrames(frame);
-    }
-    collection.shiftFrames(frame);
-    for(int i = 0; i < (50+rand()%50); ++i) {
+    } else collection.shiftFrames(frame);
+    int num_squares = 300+(rand()%100);
+    for(int i = 0; i < num_squares; ++i) {
         int x = rand()%frame.cols;
         int y = rand()%frame.rows;
-        int w = 10+rand()%(frame.cols-10);
-        int h = 10+rand()%(frame.rows-10);
+        int w = 10+rand()%((frame.cols/8)-10);
+        int h = 10+rand()%((frame.rows/8)-10);
         drawSquare(frame, x, y, w, h, collection.frames[rand()%(MAX-1)]);
     }
-    ac::FrameSep2(frame);
 }
