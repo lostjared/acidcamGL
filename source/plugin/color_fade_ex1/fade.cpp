3275f1f7c5cf9732bac0665d33be227719aa51c2
Jared Bruni <jaredbruni@gmail.com>
2022-10-02 01:50:27 -0700

    initial

---
+++ source/plugin/color_fade_ex1/fade.cpp
@@ -0,0 +1,15 @@
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    static constexpr int MAX = 8;
+    static ac::MatrixCollection<MAX> collection;
+    if(collection.empty())
+        collection.shiftFrames(frame);
+    collection.shiftFrames(frame);
+    
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+        }
+    }
+}
