095b2a7a48309ff440774202c4d1da1bc725e295
Jared Bruni <jaredbruni@gmail.com>
2022-10-02 01:59:23 -0700

    color fade ex1

--- source/plugin/color_fade_ex1/fade.cpp
+++ source/plugin/color_fade_ex1/fade.cpp
@@ -1,15 +1,24 @@
 #include"ac.h"
 
 extern "C" void filter(cv::Mat  &frame) {
-    static constexpr int MAX = 8;
-    static ac::MatrixCollection<MAX> collection;
-    if(collection.empty())
-        collection.shiftFrames(frame);
-    collection.shiftFrames(frame);
+    static int lazy = 1;
+    if(lazy == 1) {
+        lazy = 0;
+        srand(static_cast<unsigned int>(time(0)));
+    }
+    
+    static cv::Scalar color(rand()%255, rand()%255, rand()%255);
     
     for(int z = 0; z < frame.rows; ++z) {
         for(int i = 0; i < frame.cols; ++i) {
             cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+            for(int q = 0; q < 3; ++q) {
+                pixel[q] = ac::wrap_cast((pixel[q]+color[q]));
+                color[q] += 1;
+                if(color[q] >= 254) {
+                    color[q] = rand()%255;
+                }
+            }
         }
     }
 }
