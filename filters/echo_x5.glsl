0bdcebc574ad1f3b3843c74a9392339b06404fd8
Jared Bruni <jaredbruni@gmail.com>
2022-10-03 05:20:04 -0700

    shader

--- filters/echo_x5.glsl
+++ filters/echo_x5.glsl
@@ -20,7 +20,7 @@ uniform sampler2D samp;
 uniform float value_alpha_r, value_alpha_g, value_alpha_b;
 uniform float index_value;
 uniform float time_f;
-uniform vec4 inc_value;
+
 
 uniform float restore_black;
 in float restore_black_value;
@@ -29,21 +29,14 @@ void main(void)
 {
     if(restore_black_value == 1.0 && texture(samp, tc) == vec4(0, 0, 0, 1))
         discard;
-    
     color = texture(samp, tc);
-    ivec4 source = ivec4(255 * color);
-    vec2 vpos;
-    vpos[0] = 1.0-tc[0];
-    vpos[1] = tc[1];
-    vec4 color2 = texture(samp, vpos);
-    color = (0.5 * color) + (0.5 * color2);
-    vec4 color2x = texture(samp, tc / 2);
-    color2x[0] = 1.0;
+    vec4 color2 = texture(samp, tc / 2);
     vec4 color3 = texture(samp, tc/ 4);
-    color3[1] = 1.0;
     vec4 color4 = texture(samp, tc/ 8);
-    color4[2] = 1.0;
-    color = (color * 0.3) + (color2x * 0.3) + (color3 * 0.3) + (color4 * 0.3) ;
+    color2[0] *= (alpha/2);
+    color3[1] *= (alpha/4);
+    color4[2] *= (alpha/8);
+    color = (color * 0.4) + (color2 * 0.4) + (color3 * 0.4) + (color4 * 0.4) ;
 }
 
 
