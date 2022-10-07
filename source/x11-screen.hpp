524ea8b4fa703b819dedaa785d63ed4ac19d8e62
Jared Bruni <jaredbruni@gmail.com>
2022-10-07 14:41:38 -0700

    screen on linux

---
+++ source/x11-screen.hpp
@@ -0,0 +1,41 @@
+// from stackoverflow
+#ifndef __X11_SS_H_
+#define __X11_SS_H_
+#ifdef __linux__
+#include<opencv2/opencv.hpp>
+#include <X11/Xutil.h>
+class ScreenShot
+{
+    Display* display;
+    Window root;
+    int x,y,width,height;
+    XImage* img{nullptr};
+public:
+    ScreenShot(int x, int y, int width, int height):
+        x(x),
+        y(y),
+        width(width),
+        height(height)
+    {
+        display = XOpenDisplay(nullptr);
+        root = DefaultRootWindow(display);
+    }
+
+    void operator() (cv::Mat& cvImg)
+    {
+        if(img != nullptr)
+            XDestroyImage(img);
+        img = XGetImage(display, root, x, y, width, height, AllPlanes, ZPixmap);
+        cvImg = cv::Mat(height, width, CV_8UC4, img->data);
+    }
+
+    ~ScreenShot()
+    {
+        if(img != nullptr)
+            XDestroyImage(img);
+        XCloseDisplay(display);
+    }
+};
+
+#endif
+#endif
