365113ac96226b9beeda86e2cb78160edfbb7aa4
Jared Bruni <jaredbruni@gmail.com>
2022-10-11 03:12:36 -0700

    frame_x2

--- source/plugin/frame_x2/frame.cpp
+++ source/plugin/frame_x2/frame.cpp
@@ -1,15 +1,27 @@
 #include"ac.h"
 
 extern "C" void filter(cv::Mat  &frame) {
-    static constexpr int MAX = 8;
+    static constexpr int MAX = 4;
     static ac::MatrixCollection<MAX> collection;
-    if(collection.empty())
+    if(collection.empty()) {
+        srand(static_cast<unsigned int>(time(0)));
         collection.shiftFrames(frame);
-    collection.shiftFrames(frame);
+    } else
+        collection.shiftFrames(frame);
+    
     
     for(int z = 0; z < frame.rows; ++z) {
         for(int i = 0; i < frame.cols; ++i) {
             cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+            
+            for(int q = 1; q < 3; ++q) {
+                int off_i = AC_GetFX(frame.cols-1, i, (frame.cols*q)/2);
+                cv::Vec3b &pix = collection.frames[q].at<cv::Vec3b>(z, off_i);
+            
+                for(int j = 0; j < 3; ++j) {
+                    pixel[j] = ac::wrap_cast((0.5 * pixel[j]) + (0.5 * pix[j]));
+                }
+            }
         }
     }
 }
