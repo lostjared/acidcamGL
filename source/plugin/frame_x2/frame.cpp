f0b1975a87bb92903d1047f88ad8fb6a7dc9ac95
Jared Bruni <jaredbruni@gmail.com>
2022-10-11 03:37:31 -0700

    changed it

--- source/plugin/frame_x2/frame.cpp
+++ source/plugin/frame_x2/frame.cpp
@@ -8,20 +8,34 @@ extern "C" void filter(cv::Mat  &frame) {
         collection.shiftFrames(frame);
     } else
         collection.shiftFrames(frame);
+
+    static int off = 0;
     
     
-    for(int z = 0; z < frame.rows; ++z) {
-        for(int i = 0; i < frame.cols; ++i) {
-            cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+    for(int z = 0; z < frame.rows; z += 16) {
+        
+        if(++off > (MAX-1))
+            off = 0;
+        
+        
+        
+        for(int i = 0; i < frame.cols; i += 16) {
+        
+            if(++off > (MAX-1))
+                off = 0;
             
-            for(int q = 1; q < 3; ++q) {
-                int off_i = AC_GetFX(frame.cols-1, i, frame.cols+(q*(frame.cols/4)));
-                cv::Vec3b &pix = collection.frames[q].at<cv::Vec3b>(z, off_i);
+            if(rand()%2 != 0) continue;
             
-                for(int j = 0; j < 3; ++j) {
-                    pixel[j] = ac::wrap_cast((0.5 * pixel[j]) + (0.5 * pix[j]));
+            for(int x = 0; x < 16 && i+x < frame.cols; ++x) {
+                for(int y = 0; y < 16 && z+y < frame.rows; ++y) {
+                    cv::Vec3b &pixel = ac::pixelAt(frame, z+y,i+x);
+
+                    cv::Vec3b &pix = collection.frames[off].at<cv::Vec3b>(z+y, i+x);
+                    
+                    pixel = pix;
                 }
             }
+            
         }
     }
 }
