76a50bf9147f27cef555c28df86cd0680cea208f
Jared Bruni <jaredbruni@gmail.com>
2022-10-12 02:19:52 -0700

    init

---
+++ source/plugin/frame_x3/blank.cpp
@@ -0,0 +1,18 @@
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    static constexpr int MAX = 8;
+    static ac::MatrixCollection<MAX> collection;
+    if(collection.empty()) {
+        srand(static_cast<unsigned int>(time(0)));
+        collection.shiftFrames(frame);
+    }
+    else
+    collection.shiftFrames(frame);
+    
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+        }
+    }
+}
