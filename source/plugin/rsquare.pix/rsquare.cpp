fb2e6dc7c582773622c296b4379298848b87a01f
Jared Bruni <jaredbruni@gmail.com>
2022-10-15 17:04:22 -0700

    pix

--- source/plugin/rsquare.pix/rsquare.cpp
+++ source/plugin/rsquare.pix/rsquare.cpp
@@ -1,10 +1,13 @@
 #include"ac.h"
 
+
 void drawSquare(cv::Mat &frame, int x, int y, int w, int h, cv::Mat &off) {
-    for(int i = x; i < w; ++i) {
-        for(int z = y; z < h; ++z) {
+    for(int i = x; i < x+w && i < frame.cols; ++i) {
+        for(int z = y; z < y+h && z < frame.rows; ++z) {
             cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
-            pixel = off.at<cv::Vec3b>(z, i);
+            cv::Vec3b &pix = off.at<cv::Vec3b>(z, i);
+            for(int q = 0; q < 3; ++q)
+                pixel[q] = ac::wrap_cast((0.5 * pixel[q]+(0.5 * pix[q])));
         }
     }
 }
@@ -17,12 +20,11 @@ extern "C" void filter(cv::Mat  &frame) {
         collection.shiftFrames(frame);
     }
     collection.shiftFrames(frame);
-    for(int i = 0; i < (50+rand()%50); ++i) {
+    for(int i = 0; i < ((frame.rows * frame.cols)/32)/32; ++i) {
         int x = rand()%frame.cols;
         int y = rand()%frame.rows;
-        int w = 10+rand()%(frame.cols-10);
-        int h = 10+rand()%(frame.rows-10);
+        int w = rand()%32;
+        int h = rand()%32;
         drawSquare(frame, x, y, w, h, collection.frames[rand()%(MAX-1)]);
     }
-    ac::FrameSep2(frame);
 }
