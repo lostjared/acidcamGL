00d96592ead398859c52df6855b9cc44e29b4096
Jared Bruni <jaredbruni@gmail.com>
2022-10-24 05:16:11 -0700

    unknown6

--- source/plugin/unknown6/unknown.cpp
+++ source/plugin/unknown6/unknown.cpp
@@ -10,12 +10,46 @@ extern "C" void filter(cv::Mat  &frame) {
     else
         collection.shiftFrames(frame);
     
+    static int dir = 1;
     static int offset = 0;
+    static int nw = frame.cols;
+    static int nh = frame.rows;
+    
     for(int z = 0; z < frame.rows; ++z) {
         for(int i = 0; i < frame.cols; ++i) {
             cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+            int offx = AC_GetFX(frame.cols-1, i, nw);
+            int offy = AC_GetFZ(frame.rows-1, z, nh);
+            cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(offy, offx);
+            
+            for(int q = 0; q < 3; ++q) {
+                pixel[q] = ac::wrap_cast((0.5 * pixel[q]) + (0.5 * pix[q]));
+            }
             
         }
     }
  
-
+    if(dir == 1) {
+        if(++offset > (MAX-1)) {
+            offset = MAX-1;
+            dir = 0;
+        }
+        
+        nw += (rand()%5);
+        nh += (rand()%5);
+        
+        if(nw > frame.cols+(frame.cols/32)) {
+            nw = frame.cols;
+        }
+        
+        if(nh > frame.rows+(frame.rows/32)) {
+            nh = frame.rows;
+        }
+        
+    } else {
+        if(--offset <= 0) {
+            offset = 0;
+            dir = 1;
+        }
+    }
+}
