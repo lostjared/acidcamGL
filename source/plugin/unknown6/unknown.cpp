8c2e9348e671d6c2af0b23895744999109a8fa40
Jared Bruni <jaredbruni@gmail.com>
2022-10-24 05:08:44 -0700

    update

--- source/plugin/unknown6/unknown.cpp
+++ source/plugin/unknown6/unknown.cpp
@@ -11,34 +11,11 @@ extern "C" void filter(cv::Mat  &frame) {
         collection.shiftFrames(frame);
     
     static int offset = 0;
-    static int nw = frame.cols;
-    static int nh = frame.rows;
-    
     for(int z = 0; z < frame.rows; ++z) {
         for(int i = 0; i < frame.cols; ++i) {
             cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
-            int offx = AC_GetFX(frame.cols-1, i, nw);
-            int offy = AC_GetFZ(frame.rows-1, z, nh);
-            cv::Vec3b &cpix = collection.frames[offset].at<cv::Vec3b>(offy, offx);
             
-            pixel = cpix;
-        }
-        
-        if((rand()%10) == 0) {
-            nw += 1;
-            nh += 1+(rand()%5);
         }
     }
-    
-    if(nw > frame.cols+25) {
-        nw = frame.cols;
-    }
-    if(nh > frame.rows+25) {
-        nh = frame.rows;
-    }
-    
-    if(++offset > (MAX-1)) {
-        offset = 0;
-    }
-}
+ 
 
