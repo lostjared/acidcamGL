64d7a772fb3a5331d24593750c67cab98fe009ea
Jared Bruni <jaredbruni@gmail.com>
2022-10-25 08:40:19 -0700

    unknown8

--- source/plugin/unknown8/unknown.cpp
+++ source/plugin/unknown8/unknown.cpp
@@ -1,7 +1,7 @@
 #include"ac.h"
 
 extern "C" void filter(cv::Mat  &frame) {
-    static constexpr int MAX = 12;
+    static constexpr int MAX = 8;
     static ac::MatrixCollection<MAX> collection;
     if(collection.empty()) {
         srand(static_cast<unsigned int>(time(0)));
@@ -12,15 +12,11 @@ extern "C" void filter(cv::Mat  &frame) {
     
     static int dir = 1;
     static int offset = 0;
-    static int nw = frame.cols;
-    static int nh = frame.rows;
     
     for(int z = 0; z < frame.rows; ++z) {
         for(int i = 0; i < frame.cols; ++i) {
             cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
-            int offx = AC_GetFX(frame.cols-1, i, nw);
-            int offy = AC_GetFZ(frame.rows-1, z, nh);
-            cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(offy, offx);
+            cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(z, i);
             
             for(int q = 0; q < 3; ++q) {
                 pixel[q] = ac::wrap_cast((0.5 * pixel[q]) + (0.5 * pix[q]));
@@ -30,28 +26,29 @@ extern "C" void filter(cv::Mat  &frame) {
     }
  
     if(dir == 1) {
+        static int wait = 0;
+        static int timeout = rand()%10;
+        
         if(++offset > (MAX-1)) {
-            offset = MAX-1;
+            offset = 0;
+        }
+        
+        if(++wait > timeout) {
+            wait = 0;
+            timeout = rand()%10;
             dir = 0;
-        }
-        nw += (rand()%15);
-        nh += (rand()%15);
-        if(nw > frame.cols+(frame.cols/16)) {
-            nw = frame.cols;
-        }
-        if(nh > frame.rows+(frame.rows/16)) {
-            nh = frame.rows;
         }
     } else {
         static int wait = 0;
         static int timeout = rand()%10;
+        if(--offset <= 0) {
+            offset = MAX-1;
+        }
+
         if(++wait > timeout) {
             wait = 0;
             timeout = rand()%10;
-            if(--offset <= 0) {
-                offset = 0;
-                dir = 1;
-            }
+            dir = 1;
         }
     }
 }
