f6367efbb33b1284adeaf43fc1ac1e8492b1669e
Jared Bruni <jaredbruni@gmail.com>
2022-11-02 20:14:30 -0700

    added but disabled as will cause memory error

--- source/plugin/unknown11/unknown.cpp
+++ source/plugin/unknown11/unknown.cpp
@@ -7,34 +7,32 @@ extern "C" void filter(cv::Mat  &frame) {
         srand(static_cast<unsigned int>(time(0)));
         collection.shiftFrames(frame);
     }
-    else if(rand()%5 == 0)
+    else
         collection.shiftFrames(frame);
     static int dir = 1;
     static int offset = 0;
+    static int div = 2;
+    static int size_y = frame.rows/2;
     
-    frame = collection.frames[offset].clone();
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            
+            int cy = AC_GetFZ(frame.rows-1, z, size_y);
+            
+            cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(cy, i);
+            for(int q = 0; q < 3; ++q) {
+                pixel[q] = ac::wrap_cast((0.2 * pixel[q]) + (0.8 * pix[q]));
+            }
+            
+        }
+        size_y ++;
+        if(size_y > frame.rows*2)
+            size_y = frame.rows/4;
+    }
     
-    if(dir == 1) {
-        static int wait = 0;
-        static int timeout = rand()%10;
-        if(++offset > (MAX-1)) {
-            offset = 0;
-        }
-        if(++wait > timeout) {
-            wait = 0;
-            timeout = rand()%10;
-            dir = 0;
-        }
-    } else {
-        static int wait = 0;
-        static int timeout = rand()%10;
-        if(--offset <= 0) {
-            offset = MAX-1;
-        }
-        if(++wait > timeout) {
-            wait = 0;
-            timeout = rand()%10;
-            dir = 1;
-        }
+    if(++offset > (MAX-1)) {
+        offset = 0;
     }
 }
