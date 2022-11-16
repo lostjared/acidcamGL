bfd3c9e36c7e622cbb70167baa172696dfecb520
Jared Bruni <jaredbruni@gmail.com>
2022-11-15 22:11:36 -0800

    unknown29

--- source/plugin/unknown29/unknown.cpp
+++ source/plugin/unknown29/unknown.cpp
@@ -14,21 +14,29 @@ extern "C" void filter(cv::Mat  &frame) {
     static int div = 2;
     static int size_y = frame.rows/16;
     
-    for(int z = 0; z < frame.rows; ++z) {
-        for(int i = 0; i < frame.cols; ++i) {
-            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
-            int cy = AC_GetFZ(frame.rows-1, z, size_y);
-            if(cy >= 0 && cy < frame.rows && i >= 0 && i < frame.cols) {
-                cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(cy, i);
-                pixel = pix;
-            }
-        }
-        size_y ++;
-        if(size_y > frame.rows*2)
-            size_y = frame.rows/16;
-    }
+    static int wait = rand()%30;
+    static int cnt = 0;
     
-    if(++offset > (MAX-1)) {
-        offset = 0;
+    if(++cnt > wait) {
+        cnt = 0;
+        wait = rand()%10;
+        
+        for(int z = 0; z < frame.rows; ++z) {
+            for(int i = 0; i < frame.cols; ++i) {
+                cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+                int cy = AC_GetFZ(frame.rows-1, z, size_y);
+                if(cy >= 0 && cy < frame.rows && i >= 0 && i < frame.cols) {
+                    cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(cy, i);
+                    pixel = pix;
+                }
+            }
+            size_y ++;
+            if(size_y > frame.rows*2)
+                size_y = frame.rows/16;
+        }
+        
+        if(++offset > (MAX-1)) {
+            offset = 0;
+        }
     }
 }
