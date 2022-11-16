6c3964ac7c3d4ab9b77dfc3572210e36297545e7
Jared Bruni <jaredbruni@gmail.com>
2022-11-16 11:47:10 -0800

    unknown30

--- source/plugin/unknown30/unknown.cpp
+++ source/plugin/unknown30/unknown.cpp
@@ -13,22 +13,29 @@ extern "C" void filter(cv::Mat  &frame) {
     static int offset = 0;
     static int div = 2;
     static int size_x = frame.cols/16;
+    static int wait = rand()%30;
+    static int counter = 0;
     
-    for(int z = 0; z < frame.rows; ++z) {
-        for(int i = 0; i < frame.cols; ++i) {
-            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
-            int cx = AC_GetFX(frame.cols-1, i, size_x);
-            if(cx >= 0 && cx < frame.cols && z >= 0 && z < frame.rows) {
-                cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(z, cx);
-                pixel = pix;
+    if(++counter > wait) {
+        counter = 0;
+        wait = rand()%10;
+        
+        for(int z = 0; z < frame.rows; ++z) {
+            for(int i = 0; i < frame.cols; ++i) {
+                cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+                int cx = AC_GetFX(frame.cols-1, i, size_x);
+                if(cx >= 0 && cx < frame.cols && z >= 0 && z < frame.rows) {
+                    cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(z, cx);
+                    pixel = pix;
+                }
             }
+            size_x ++;
+            if(size_x > frame.cols*2)
+                size_x = frame.cols/16;
         }
-        size_x ++;
-        if(size_x > frame.cols*2)
-            size_x = frame.cols/16;
-    }
-    
-    if(++offset > (MAX-1)) {
-        offset = 0;
+        
+        if(++offset > (MAX-1)) {
+            offset = 0;
+        }
     }
 }
