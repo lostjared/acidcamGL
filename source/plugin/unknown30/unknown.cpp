10a7510306be2372294fdb13411ea5ed1ec6f8f6
Jared Bruni <jaredbruni@gmail.com>
2022-11-16 11:45:37 -0800

    Init

---
+++ source/plugin/unknown30/unknown.cpp
@@ -0,0 +1,34 @@
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    static constexpr int MAX = 8;
+    static ac::MatrixCollection<MAX> collection;
+    if(collection.empty()) {
+        srand(static_cast<unsigned int>(time(0)));
+        collection.shiftFrames(frame);
+    }
+    else
+        collection.shiftFrames(frame);
+    static int dir = 1;
+    static int offset = 0;
+    static int div = 2;
+    static int size_x = frame.cols/16;
+    
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            int cx = AC_GetFX(frame.cols-1, i, size_x);
+            if(cx >= 0 && cx < frame.cols && z >= 0 && z < frame.rows) {
+                cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(z, cx);
+                pixel = pix;
+            }
+        }
+        size_x ++;
+        if(size_x > frame.cols*2)
+            size_x = frame.cols/16;
+    }
+    
+    if(++offset > (MAX-1)) {
+        offset = 0;
+    }
+}
