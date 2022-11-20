c9ebc4675ccdbc93fdd74c3362de31341a2db1a3
Jared Bruni <jaredbruni@gmail.com>
2022-11-20 14:45:18 -0800

    unknown31

--- source/plugin/unknown31/unknown.cpp
+++ source/plugin/unknown31/unknown.cpp
@@ -11,31 +11,38 @@ extern "C" void filter(cv::Mat  &frame) {
         collection.shiftFrames(frame);
     static int dir = 1;
     static int offset = 0;
-    static int div = 2;
-    static int size_x = frame.cols/16;
-    static int wait = rand()%30;
-    static int counter = 0;
+    int size_y = 0;
     
-    if(++counter > wait) {
-        counter = 0;
-        wait = rand()%10;
-        
-        for(int z = 0; z < frame.rows; ++z) {
-            for(int i = 0; i < frame.cols; ++i) {
-                cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
-                int cx = AC_GetFX(frame.cols-1, i, size_x);
-                if(cx >= 0 && cx < frame.cols && z >= 0 && z < frame.rows) {
-                    cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(z, cx);
-                    pixel = pix;
-                }
+    size_y = rand()%frame.rows;
+    
+    for(int z = 0; z < frame.rows/2; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            int cy = AC_GetFZ(frame.rows-1, z, size_y);
+            if(cy >= 0 && cy < frame.rows) {
+                cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(cy, i);
+                pixel = pix;
             }
-            size_x ++;
-            if(size_x > frame.cols*2)
-                size_x = frame.cols/16;
         }
-        
-        if(++offset > (MAX-1)) {
-            offset = 0;
-        }
+        size_y ++;
     }
+    
+    for(int z = frame.rows/2; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            int cy = AC_GetFZ(frame.rows-1, z, size_y);
+            
+            if(cy >= 0 && cy < frame.rows) {
+                cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(cy, i);
+                pixel = pix;
+            }
+        }
+        size_y --;
+    }
+    
+    
+    if(++offset > (MAX-1)) {
+        offset = 0;
+    }
+    
 }
