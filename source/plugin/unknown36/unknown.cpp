05d1b4bd7b718346650b643956490ac942fcf3e0
Jared Bruni <jaredbruni@gmail.com>
2022-11-23 13:12:36 -0800

    unknown36

--- source/plugin/unknown36/unknown.cpp
+++ source/plugin/unknown36/unknown.cpp
@@ -14,37 +14,62 @@ extern "C" void filter(cv::Mat  &frame) {
     if(flash) {
         static int dir = 1;
         static int offset = 0;
-        int size_y = 0;
+        static int size_y = frame.rows/16;
+        static int size_x = 2;
+        static int dir2 = 1;
+        static int direction = 1;
         
-        size_y = rand()%frame.rows;
-        
-        for(int z = 0; z < frame.rows/2; ++z) {
+        for(int z = 0; z < frame.rows; ++z) {
             for(int i = 0; i < frame.cols; ++i) {
                 cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
-                int cy = AC_GetFZ(frame.rows-1, z, size_y);
-                if(cy >= 0 && cy < frame.rows) {
-                    cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(cy, i);
-                    pixel = pix;
-                }
-            }
-            size_y ++;
-        }
-        
-        for(int z = frame.rows/2; z < frame.rows; ++z) {
-            for(int i = 0; i < frame.cols; ++i) {
-                cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
-                int cy = AC_GetFZ(frame.rows-1, z, size_y);
+                int cy = AC_GetFZ(frame.rows-1, i, size_y);
+                int cx = AC_GetFX(frame.cols-1, z, size_x);
                 
-                if(cy >= 0 && cy < frame.rows) {
-                    cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(cy, i);
+                if(cy >= 0 && cy < frame.rows && cx >= 0 && cx < frame.cols) {
+                    cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(cy, cx);
                     pixel = pix;
                 }
             }
-            size_y --;
+            
+            if(dir == 1) {
+                size_y += rand()%10;
+                if(size_y > frame.rows*2) {
+                    dir = 0;
+                    size_y = frame.rows*2;
+                }
+            } else {
+                size_y -= rand()%10;
+                if(size_y <= 2) {
+                    dir = 1;
+                    size_y = 2;
+                }
+            }
+            
+            if(dir2 == 1) {
+                size_x += rand()%10;
+                if(size_x > frame.rows*4) {
+                    dir2 = 0;
+                    size_x = frame.rows*4;
+                }
+            } else {
+                size_x -= rand()%10;
+                if(size_x <= 2) {
+                    dir2 = 1;
+                    size_x = 2;
+                }
+            }
         }
         
-        if(++offset > (MAX-1)) {
-            offset = 0;
+        if(direction == 1) {
+            if(++offset > (MAX-1)) {
+                direction = 0;
+                offset = MAX-1;
+            }
+        } else {
+            if(--offset <= 0) {
+                direction = 1;
+                offset = 0;
+            }
         }
         
     } else {
@@ -71,6 +96,6 @@ extern "C" void filter(cv::Mat  &frame) {
             offset = 0;
         }
     }
-
+    
     flash = (flash == true) ? false : true;
 }
