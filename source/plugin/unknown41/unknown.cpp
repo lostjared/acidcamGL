5f160cdc16ebbd32d822cd8a38537e771e2103ce
Jared Bruni <jaredbruni@gmail.com>
2022-11-26 15:20:17 -0800

    unknown41

---
+++ source/plugin/unknown41/unknown.cpp
@@ -0,0 +1,33 @@
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    static constexpr int MAX = 8;
+    static ac::MatrixCollection<MAX> collection;
+    if(collection.empty()) {
+        srand(static_cast<unsigned int>(time(0)));
+        collection.shiftFrames(frame);
+    }
+    else
+        collection.shiftFrames(frame);
+
+    int row = 0;
+    static int offset = 0;
+    
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            
+            cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(z, i);
+            
+            
+            pixel[row] ^= pix[row];
+        }
+        if(++row > 2)
+            row = 0;
+        if(++offset > (MAX-1))
+            offset = rand()%(MAX-1);
+
+    }
+    
+}
