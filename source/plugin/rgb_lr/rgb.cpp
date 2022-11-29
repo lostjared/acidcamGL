9a30cc4c5f19863fab7bbf223286b2769d8cd92d
Jared Bruni <jaredbruni@gmail.com>
2022-11-29 14:04:59 -0800

    Updated

--- source/plugin/rgb_lr/rgb.cpp
+++ source/plugin/rgb_lr/rgb.cpp
@@ -10,36 +10,41 @@ extern "C" void filter(cv::Mat  &frame) {
     else
         collection.shiftFrames(frame);
     
-    static int start_x[2] = {0};
-    static int start_y[2] = {0};
+    static int start_x[2] = {0, frame.cols};
+    static int start_y[2] = {0, frame.rows};
     static int offset = 0;
     
     for(int z = 0; z < frame.rows; ++z) {
         for(int i = 0; i < frame.cols; ++i) {
             cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
             int xpos = start_x[0]+i;
+            int xpos2 = i-start_x[1];
+            
             if(xpos >= 0 && xpos < frame.cols) {
                 cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(z, xpos);
-                pixel[0] = pix[0];
-            }
-            int xpos2 = i-start_x[1];
+                pixel[0] = ac::wrap_cast((0.5 * pixel[0]) + (0.5 * pix[0]));
+            } else
             if(xpos2 >= 0 && xpos2 < frame.cols) {
                 cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(z, xpos2);
-                pixel[1] = pix[1];
+                pixel[0] = ac::wrap_cast((0.5 * pixel[0]) + (0.5 * pix[0]));
             }
             
             int ypos = start_y[0]+z;
+            int ypos2 = z-start_y[1];
+
             if(ypos >= 0 && ypos < frame.rows) {
                 cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(ypos, i);
-                pixel[2] = pix[2];
-            }
-            int ypos2 = z-start_y[1];
+                pixel[1] = ac::wrap_cast((0.5 * pixel[1]) + (0.5 * pix[1]));
+
+            } else
             if(ypos2 >= 0 && ypos2 < frame.rows) {
                 cv::Vec3b &pix = collection.frames[offset].at<cv::Vec3b>(ypos2, i);
-                pixel[0] = pix[0];
+                pixel[2] = ac::wrap_cast((0.5 * pixel[2]) + (0.5 * pix[2]));
+
             }
         }
     }
+    
     start_x[0] += 50;
     if(start_x[0] > frame.cols) {
         start_x[0] = 0;
