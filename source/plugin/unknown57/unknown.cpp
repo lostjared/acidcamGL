76550d22b4c928a13949195fb3600f5643eaa500
Jared Bruni <jaredbruni@gmail.com>
2022-12-08 13:38:26 -0800

    unknown57

---
+++ source/plugin/unknown57/unknown.cpp
@@ -0,0 +1,22 @@
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    ac::MatrixCollection<2> collection;
+    collection.shiftFrames(frame);
+    static int offset_x = frame.cols;
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = frame.cols; i > 0; i--) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            int off_x = AC_GetFX(frame.cols-1, i, offset_x);
+            offset_x++;
+            if(off_x >= 0 && off_x < frame.cols) {
+                pixel = collection.frames[1].at<cv::Vec3b>(z, off_x);
+            }
+        }
+        
+        if(offset_x > (frame.cols*4)) {
+            offset_x = frame.cols;
+        }
+    }
+
+}
