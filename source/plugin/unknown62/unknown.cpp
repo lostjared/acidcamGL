c227a5071ee22f9a648ef7459180c2748a9a970f
Jared Bruni <jaredbruni@gmail.com>
2022-12-10 17:45:28 -0800

    unknown62

---
+++ source/plugin/unknown62/unknown.cpp
@@ -0,0 +1,26 @@
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    int off_x = rand()%frame.cols;
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            
+            int xpos = AC_GetFX(frame.cols, i, off_x);
+            if(xpos >= 0 && xpos < frame.cols) {
+                cv::Vec3b &pix = frame.at<cv::Vec3b>(z, xpos);
+                for(int q = 0; q < 3; ++q) {
+                    pixel[q] = ac::wrap_cast((0.5 * pixel[q]) + (0.5 * pix[q]));
+                }
+            }
+            
+        }
+        if((z%5) == 0) {
+            ++off_x;
+            if(off_x > frame.cols)
+                off_x = rand()%frame.cols;
+        }
+    }
+    
+    
+}
