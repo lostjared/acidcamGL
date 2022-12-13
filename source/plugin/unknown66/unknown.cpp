807f214ccb8bbf4c33f655b2e3a296c28eda09e8
Jared Bruni <jaredbruni@gmail.com>
2022-12-13 13:42:37 -0800

    unknown66

---
+++ source/plugin/unknown66/unknown.cpp
@@ -0,0 +1,15 @@
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    
+    for(int z = 0; z < frame.rows; z += 2) {
+        int col_start = frame.cols/4+rand()%50;
+        int col_space = rand()%frame.cols/2;
+        for(int i = col_start; i < col_start+col_space; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            pixel[0] ^= 150+rand()%50;
+            pixel[1] ^= 150+rand()%50;
+            pixel[2] ^= 150+rand()%50;
+        }
+    }
+}
