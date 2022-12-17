63a9a02ef9f449ba0588768cc975ddfcf4b0458c
Jared Bruni <jaredbruni@gmail.com>
2022-12-17 13:25:47 -0800

    line_down_and

---
+++ source/plugin/line_down_and/line_down.cpp
@@ -0,0 +1,58 @@
+#include"ac.h"
+#include<memory>
+
+
+struct Impl {
+    int *rows = nullptr;
+    int *row_speed = nullptr;
+    
+    Impl() = default;
+    ~Impl() {
+        delete [] rows;
+        delete [] row_speed;
+    }
+};
+
+
+
+extern "C" void filter(cv::Mat  &frame) {
+    static ac::MatrixCollection<2> collection;
+    collection.shiftFrames(frame);
+    static int row_w = 0;
+    static Impl row;
+    
+    if(row_w == 0 || row_w != frame.cols) {
+        
+        if(row.rows != nullptr) {
+            delete [] row.rows;
+            delete [] row.row_speed;
+        }
+        
+        row.rows = new int[frame.cols+1];
+        row.row_speed =new int[frame.cols+1];
+        for(int i = 0; i < frame.cols; ++i) {
+            row.rows[i] = rand()%50;
+            row.row_speed[i] = 2+rand()%25;
+        }
+        row_w = frame.cols;
+    }
+    
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            int row_y = row.rows[i]+z;
+            if(row_y >= 0 && row_y < frame.rows) {
+                cv::Vec3b &pix = collection.frames[1].at<cv::Vec3b>(row_y, i);
+                for(int q = 0; q < 3; ++q)
+                    pixel[q] &= pix[q];
+            }
+        }
+    }
+    
+    for(int i = 0; i < frame.cols; ++i) {
+        row.rows[i] += row.row_speed[i];
+        if(row.rows[i] > frame.rows) {
+            row.rows[i] = rand()%50;
+        }
+    }
+}
