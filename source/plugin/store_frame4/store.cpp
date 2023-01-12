7db2c27557345dff2c8db032ea213a5eb81581eb
Jared Bruni <jaredbruni@gmail.com>
2023-01-12 13:59:54 -0800

    store_frame4

---
+++ source/plugin/store_frame4/store.cpp
@@ -0,0 +1,46 @@
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    static constexpr int MAX = 8;
+    static ac::MatrixCollection<MAX> collection;
+    if(collection.empty()) {
+        srand(static_cast<unsigned int>(time(0)));
+        collection.shiftFrames(frame);
+    }
+    
+    static int counter = 0;
+    if(++counter > 2) {
+        counter = 0;
+        collection.shiftFrames(frame);
+    }
+    
+    static double alpha = 1.0;
+    
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+            cv::Vec3b *off[3];
+            off[0] = &collection.frames[1].at<cv::Vec3b>(z, i);
+            off[1] = &collection.frames[3].at<cv::Vec3b>(z, i);
+            off[2] = &collection.frames[7].at<cv::Vec3b>(z, i);
+            cv::Vec3b pix = pixel;
+            for(int q = 0; q < 3; ++q) {
+                pixel[q] = (*off[q])[q];
+                pixel[q] = ac::wrap_cast((0.5 * pixel[q]) + (0.5 * pix[q]));
+            }
+        }
+    }
+    
+    static int dir = 1;
+    
+    if(dir == 1) {
+        alpha += 0.05;
+        if(alpha > 5)
+            dir = 0;
+    }
+    else {
+        alpha -= 0.05;
+        if(alpha <= 1.5)
+            dir = 1;
+    }
+}
