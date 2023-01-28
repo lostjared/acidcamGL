51ae71bfa9b9534a1c2ba64a7e5ea67f539229a2
Jared Bruni <jaredbruni@gmail.com>
2023-01-28 06:16:33 -0800

    added check

--- source/plugin/unknown66/unknown.cpp
+++ source/plugin/unknown66/unknown.cpp
@@ -4,7 +4,7 @@ extern "C" void filter(cv::Mat  &frame) {
     for(int z = 0; z < frame.rows; z += 2) {
         int col_start = frame.cols/4+rand()%50;
         int col_space = rand()%frame.cols/2;
-        for(int i = col_start; i < col_start+col_space; ++i) {
+        for(int i = col_start; i < col_start+col_space && i < frame.cols; ++i) {
             cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
             pixel[0] ^= 150+rand()%50;
             pixel[1] ^= 150+rand()%50;
