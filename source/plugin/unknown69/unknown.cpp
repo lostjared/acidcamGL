ea927078f3322e641ff14566c73119011d0c484a
Jared Bruni <jaredbruni@gmail.com>
2023-06-10 18:45:07 -0700

    color increase

--- source/plugin/unknown69/unknown.cpp
+++ source/plugin/unknown69/unknown.cpp
@@ -7,13 +7,15 @@ extern "C" void filter(cv::Mat  &frame) {
     for(int z = 0; z < frame.rows; z++) {
         for(int i = 0; i < frame.cols; i++) {
             cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
-            auto value = ac::wrap_cast(alpha * pixel[col]);
-            for(int q = 0; q < 3; ++q)
-                pixel[q] = pixel[q]^value;
+            unsigned char value[3];
+            for(int q = 0; q < 3; ++q) {
+                value[q] = ac::wrap_cast(alpha * pixel[q]);
+                pixel[q] = pixel[q]^value[q];
+            }
            
         }
     }
-    alpha += 0.01;
+    alpha += 0.001;
     if(alpha >= 3.0)
         alpha = 1.0;
     ++col;
