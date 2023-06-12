6f14f200f181aa78d8fe3377dd0c0d570a2f08f9
Jared Bruni <jaredbruni@gmail.com>
2023-06-11 21:28:10 -0700

    updated Makefile index

---
+++ source/plugin/unknown71/unknown.cpp
@@ -0,0 +1,32 @@
+// strobe
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    static double alpha = 1.0;
+    
+    for(int z = 0; z < frame.rows; z++) {
+        for(int i = 0; i < frame.cols; i++) {
+            cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+            unsigned char value[3];
+            for(int q = 0; q < 3; ++q) {
+                value[q] = ac::wrap_cast(alpha * pixel[q]);
+                pixel[q] = pixel[q]^value[q];
+            }
+           
+        }
+    }
+    static int dir = 1;
+    if(dir == 1) {
+        alpha += 0.01;
+        if(alpha >= 3.0) {
+            alpha = 1.0;
+            dir = 0;
+        }
+    } else {
+        alpha -= 0.01;
+        if(alpha <= 1) {
+            alpha = 1.0;
+            dir = 1;
+        }
+    }
+}
