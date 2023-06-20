2948fc1369cdc420a0ba9a19ea96e24cda78a9e1
Jared Bruni <jaredbruni@gmail.com>
2023-06-20 04:06:42 -0700

    color xor (variable paths)

---
+++ source/plugin/unknown81/unknown.cpp
@@ -0,0 +1,61 @@
+// color xor (variable)
+#include"ac.h"
+
+extern "C" void filter(cv::Mat  &frame) {
+    static int lazy = 0;
+    static double alpha[3] = {
+        static_cast<double>(rand()%4),
+        static_cast<double>(rand()%4),
+        static_cast<double>(rand()%4)
+    };
+  
+    if(lazy == 0) {
+        srand(static_cast<unsigned int>(time(0)));
+        alpha[0] = static_cast<double>(rand()%4);
+        alpha[1] = static_cast<double>(rand()%4);
+        alpha[2] = static_cast<double>(rand()%4);
+        lazy = 1;
+    }
+    
+    for(int z = 0; z < frame.rows; z++) {
+        for(int i = 0; i < frame.cols; i++) {
+            cv::Vec3b &pixel = ac::pixelAt(frame, z, i);
+            unsigned char value[3];
+            for(int q = 0; q < 3; ++q) {
+                value[q] = ac::wrap_cast(alpha[q] * pixel[q]);
+                pixel[q] = pixel[q]^value[q];
+            }
+            
+        }
+    }
+    static int dir[3] = {rand()%2, rand()%2, rand()%2};
+    
+    for(int q = 0; q < 3; ++q) {
+        double r = static_cast<double>(rand()%10);
+        if(dir[q] == 1) {
+            alpha[q] += 0.01*r;
+            if(alpha[q] >= 3.0) {
+                alpha[q] = 3.0;
+                dir[q] = rand()%5;
+            }
+        } else if (dir[q] == 2) {
+            alpha[q] -= 0.01*r;
+            if(alpha[q] <= 1) {
+                alpha[q] = 1.0;
+                dir[q] = rand()%5;
+            }
+        } else if(dir[q] == 3) {
+            alpha[q] += 0.01*r;
+            if(alpha[q] >= 6.0) {
+                alpha[q] = 6.0;
+                dir[q] = rand()%5;
+            }
+        } else {
+            alpha[q] -= 0.01*r;
+            if(alpha[q] <= 3.0) {
+                alpha[q] = 3.0;
+                dir[q] = rand()%5;
+            }
+        }
+    }
+}
