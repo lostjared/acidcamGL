25879ecbc4910877a0b719d55a6e76a3b881c440
Jared Bruni <jaredbruni@gmail.com>
2024-03-24 07:11:31 -0700

    updated Added Material water shader

--- filters/material_water.glsl
+++ filters/material_water.glsl
@@ -1,8 +1,11 @@
+
 #version 330
 
 in vec2 tc;
 out vec4 color;
 uniform sampler2D samp;
+uniform sampler2D mat_samp;
+
 uniform float time_f;
 
 void main(void) {
@@ -13,6 +16,6 @@ void main(void) {
     ripple += sin(tc.y * wavelength + time_f * speed) * amplitude;
     vec2 rippleTC = tc + vec2(ripple, ripple);
     vec4 originalColor = texture(samp, tc);
-    vec4 rippleColor = texture(samp, rippleTC);
+    vec4 rippleColor = texture(mat_samp, rippleTC);
     color = mix(originalColor, rippleColor, 0.5);
 }
---
+++ filters/xor_increase_alpha.glsl
@@ -0,0 +1,41 @@
+#version 330
+in vec2 tc;
+out vec4 color;
+in float alpha_r;
+in float alpha_g;
+in float alpha_b;
+in float current_index;
+in float timeval;
+in float alpha;
+in vec3 vpos;
+in vec4 optx_val;
+uniform vec4 optx;
+in vec4 random_value;
+uniform vec4 random_var;
+uniform float alpha_value;
+
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform sampler2D samp;
+uniform float value_alpha_r, value_alpha_g, value_alpha_b;
+uniform float index_value;
+uniform float time_f;
+
+uniform float restore_black;
+in float restore_black_value;
+
+void main(void)
+{
+    if(restore_black_value == 1.0 && texture(samp, tc) == vec4(0, 0, 0, 1))
+        discard;
+    color = texture(samp, tc);
+    vec4 a = vec4(color*alpha);
+    for(int q = 0; q < 4;  ++q) {
+        int value = color[q] * 255;
+        int value2 = a[q] * 255;
+        int value3 = (int)value^(int)value2;
+        //color[q] = value3/255;
+    }
+}
+
+
