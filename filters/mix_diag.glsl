79ec9c0865a054037a41223b27052492630367da
Jared Bruni <jaredbruni@gmail.com>
2024-03-24 20:09:37 -0700

    new shader

---
+++ filters/mix_diag.glsl
@@ -0,0 +1,16 @@
+#version 330 core
+
+in vec2 tc;
+out vec4 color;
+uniform sampler2D samp;
+uniform float time_f;
+
+void main() {
+    
+    color = texture(samp, tc);
+    vec2 uv = tc;
+    uv.x -= 0.05;
+    uv.y -= 0.05;
+    vec4 color2 = texture(samp, uv);
+    color = mix(color, color2, 0.5);
+}
