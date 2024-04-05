b3fc58ebe07a9b4be4da2a766a4c4ae3925169d3
Jared Bruni <jaredbruni@gmail.com>
2024-04-05 02:01:22 -0700

    psyche_wave

---
+++ filters/psyche_wave.glsl
@@ -0,0 +1,18 @@
+#version 330
+in vec2 tc; 
+out vec4 color;
+uniform float time_f;
+uniform sampler2D samp;
+uniform vec2 iResolution;
+void main(void)
+{
+    vec2 normCoord = ((gl_FragCoord.xy / iResolution.xy) * 2.0 - 1.0) * vec2(iResolution.x / iResolution.y, 1.0);
+
+    float distanceFromCenter = length(normCoord);
+    float wave = sin(distanceFromCenter * 12.0 - time_f * 4.0);
+
+    vec2 tcAdjusted = tc + (normCoord * 0.301 * wave);
+
+    vec4 textureColor = texture(samp, tcAdjusted);
+    color = textureColor;
+}
