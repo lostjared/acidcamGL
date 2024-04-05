81fd63cab9b31e97323e8a2242ca072853151252
Jared Bruni <jaredbruni@gmail.com>
2024-04-05 00:54:41 -0700

    new shader

---
+++ filters/ripple_out.glsl
@@ -0,0 +1,15 @@
+#version 330
+in vec2 tc;
+out vec4 color;
+uniform float time_f;
+uniform sampler2D samp;
+uniform vec2 iResolution;
+
+void main(void)
+{
+    vec2 normPos = (gl_FragCoord.xy / iResolution.xy) * 2.0 - 1.0;
+    float dist = length(normPos);
+    float phase = sin(dist * 10.0 - time_f * 4.0);
+    vec2 tcAdjusted = tc + (normPos * 0.305 * phase);
+    color = texture(samp, tcAdjusted);
+}
