9dce2d5a486bee9ee1e435afa47647a7581b1a01
Jared Bruni <jaredbruni@gmail.com>
2024-04-05 09:36:06 -0700

    cyclone

--- filters/code_wave.glsl
+++ filters/code_wave.glsl
@@ -5,8 +5,7 @@ uniform float time_f;
 uniform sampler2D samp;
 uniform vec2 iResolution;
 
-void main(void)
-{
+void main(void) {
     float yPos = gl_FragCoord.y / iResolution.y;
     float wavePhase = sin(yPos * 10.0 + time_f * 2.0);
     vec2 tcAdjusted = tc + vec2(0, wavePhase * 0.305);
