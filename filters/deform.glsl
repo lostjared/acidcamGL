ceaf8bcd7079b27508ced7058df1ca47fb771227
Jared Bruni <jaredbruni@gmail.com>
2024-04-05 15:08:26 -0700

    deform

---
+++ filters/deform.glsl
@@ -0,0 +1,16 @@
+#version 330 core
+out vec4 color;
+in vec2 tc;
+uniform sampler2D samp;
+uniform vec2 iResolution;
+uniform float time_f;
+void main(void) {
+    vec2 normCoord = tc;
+    float warpAmount = sin(time_f);
+    vec2 warp = vec2(
+        sin(normCoord.y * 10.0 + time_f) * warpAmount,
+        cos(normCoord.x * 10.0 + time_f) * warpAmount
+    );
+    vec2 warpedCoord = normCoord + warp;
+    color = texture(samp, warpedCoord);
+}
