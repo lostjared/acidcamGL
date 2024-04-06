e351ae05fafc4388bd4bbc0d770877a7873224b9
Jared Bruni <jaredbruni@gmail.com>
2024-04-06 14:17:36 -0700

    spiral_mirror

--- filters/spiral_mirror_top.glsl
+++ filters/spiral_mirror_top.glsl
@@ -7,15 +7,20 @@ uniform float time_f;
 
 void main(void) {
     vec2 normCoord = (tc * 2.0 - 1.0) * vec2(iResolution.x / iResolution.y, 1.0);
-    normCoord.y = abs(normCoord.y);
+
+    if (normCoord.y < 0.0) {
+        normCoord.y = -normCoord.y;
+    }
     float dist = length(normCoord);
     float angle = atan(normCoord.y, normCoord.x);
-    float spiralAmount = sin(time_f);
+    float spiralAmount = time_f;
     angle += spiralAmount;
     dist = mix(dist, 1.0 - dist, sin(spiralAmount * 0.5));
     vec2 spiralCoord = vec2(cos(angle), sin(angle)) * dist;
+
     float scale = 1.0 + sin(spiralAmount * 0.2) * 0.5;
     spiralCoord *= scale;
     spiralCoord = (spiralCoord / vec2(iResolution.x / iResolution.y, 1.0) + 1.0) / 2.0;
+
     color = texture(samp, spiralCoord);
 }
