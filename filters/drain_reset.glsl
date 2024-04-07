eb46ae71a3c3d480ff8abccb895518498772e509
Jared Bruni <jaredbruni@gmail.com>
2024-04-06 23:25:42 -0700

    drain

--- filters/drain_reset.glsl
+++ filters/drain_reset.glsl
@@ -5,21 +5,17 @@ in vec2 tc;
 uniform sampler2D samp;
 uniform vec2 iResolution;
 uniform float time_f;
+
 void main(void) {
+    float loopDuration = 5.0;
+    float currentTime = mod(time_f, loopDuration);
     vec2 normCoord = (tc * 2.0 - 1.0) * vec2(iResolution.x / iResolution.y, 1.0);
     float dist = length(normCoord);
     float angle = atan(normCoord.y, normCoord.x);
-    float spiralSpeed = 2.0;
-    float inwardSpeed = 0.5;
-    float drainRadius = 8.0;
-    
-    float loopDuration = 20.0;
-    float currentTime = mod(time_f, loopDuration);
-
-    
-    angle += (1.0 - smoothstep(0.0, drainRadius, dist)) * currentTime * spiralSpeed;
-        dist *= mix(1.0, 0.0, currentTime * inwardSpeed);
-    vec2 spiralCoord = vec2(cos(angle), sin(angle)) * dist;
+    float spiralSpeed = 5.0;
+    float inwardSpeed = currentTime / loopDuration;     angle += (1.0 - smoothstep(0.0, 8.0, dist)) * currentTime * spiralSpeed;
+    dist *= 1.0 - inwardSpeed;
+    vec2 spiralCoord = vec2(cos(angle), sin(angle)) * tan(dist);
     spiralCoord = (spiralCoord / vec2(iResolution.x / iResolution.y, 1.0) + 1.0) / 2.0;
     color = texture(samp, spiralCoord);
 }
