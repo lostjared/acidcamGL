c9db20512a695947853024948d78e4787296165e
Jared Bruni <jaredbruni@gmail.com>
2024-04-21 01:32:37 -0700

    kale

---
+++ filters/focus.glsl
@@ -0,0 +1,25 @@
+#version 330 core
+out vec4 color;
+in vec2 tc;
+
+uniform sampler2D samp;
+uniform vec2 iResolution;
+uniform float time_f;
+float noise(vec2 st) {
+    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
+void main(void) {
+    vec2 uv = tc;
+    float time = time_f * 0.5;
+    float noiseValue = noise(uv * 10.0 + time);
+    vec2 heatWaveOffset = vec2(noiseValue - 0.5, 0.0) * 0.02;
+    
+    float time_t = mod(time_f, 10);
+    vec2 nuv = uv + sin(heatWaveOffset * time_t);
+
+    
+    vec4 texColor = texture(samp, nuv);
+    color = texColor;
+}
+
---
+++ filters/kale.glsl
@@ -0,0 +1,27 @@
+#version 330 core
+out vec4 color;
+in vec2 tc;
+
+uniform sampler2D samp;
+uniform vec2 iResolution;
+uniform float time_f;
+void main(void) {
+    vec2 uv = tc * iResolution / vec2(iResolution.y);
+    vec2 center = vec2(0.5, 0.5) * iResolution / vec2(iResolution.y);
+    vec2 pos = uv - center;
+    float r = length(pos);
+    float angle = atan(pos.y, pos.x);
+    float numSegments = 6.0;
+    float segmentAngle = 2.0 * 3.14159 / numSegments;
+    angle = mod(angle, segmentAngle);
+    angle = abs(angle - segmentAngle / 2.0);
+    pos.x = r * cos(angle);
+    pos.y = r * sin(angle);
+    float rotationSpeed = time_f * 0.7;
+    float cosA = cos(rotationSpeed);
+    float sinA = sin(rotationSpeed);
+    mat2 rot = mat2(cosA, -sinA, sinA, cosA);
+    pos = rot * pos;
+    vec4 texColor = texture(samp, pos + center);
+    color = texColor;
+}
