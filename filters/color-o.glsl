d26ee445b1cfff2e1946153ec8aadf6ac7784aa9
Jared Bruni <jaredbruni@gmail.com>
2024-05-13 21:18:39 -0700

    shaders

---
+++ filters/color-o.glsl
@@ -0,0 +1,23 @@
+#version 330 core
+
+in vec2 tc;
+out vec4 color;
+
+uniform sampler2D samp;
+uniform float time_f;
+
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+void main(void) {
+    vec4 ctx = texture(samp, tc);
+    float adjusted_time = time_f * 0.05;
+    float hue = mod(tc.x + tc.y * 0.5 + adjusted_time, 1.0);
+    vec3 hsvColor = vec3(hue, 1.0, 1.0);
+    vec3 smoothColor = hsv2rgb(hsvColor);
+    ctx.rgb = mix(ctx.rgb, smoothColor, 0.7);
+    color = ctx;
+}
