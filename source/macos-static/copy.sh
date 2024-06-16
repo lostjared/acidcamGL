255444ffb0fe93b3c47a35a3ddb77ebd49c9dc5f
Jared Bruni <jaredbruni@gmail.com>
2024-06-16 01:09:53 -0700

    updated

--- source/macos-static/copy.sh
+++ source/macos-static/copy.sh
@@ -2,4 +2,4 @@
 
 make clean && make -j16 && cp ./acidcamGL /Applications/acidcamGL/acidcamGL.app/Contents/MacOS/acidcamGL 
 install_name_tool -change @loader_path/../Frameworks/Syphon.framework/Versions/A/Syphon  @executable_path/../Frameworks/Syphon.framework/Versions/A/Syphon  /Applications/acidcamGL/acidcamGL.app/Contents/MacOS/acidcamGL 
-cp acidcamGL /usr/local/bin
+cp acidcamGL_midi /usr/local/bin
