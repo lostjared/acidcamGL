8540dc492bfc2cc97918d1dc0f9463725aeea097
Jared Bruni <jaredbruni@gmail.com>
2024-06-25 20:10:56 -0700

    zoom

---
+++ filters/cd_zoom_out.glsl
@@ -0,0 +1,22 @@
+#version 330 core
+out vec4 color;
+in vec2 tc;
+
+uniform sampler2D samp;
+uniform float time_f;
+uniform vec2 iResolution;
+
+void main(void) {
+    vec2 centeredCoord = tc - 0.5;
+    float angle = time_f;
+    
+    float zoom = 2.5 - abs(sin(time_f)) * 4.0;
+    centeredCoord *= zoom;
+    
+    vec2 rotatedCoord;
+    rotatedCoord.x = centeredCoord.x * cos(angle) - centeredCoord.y * sin(angle);
+    rotatedCoord.y = centeredCoord.x * sin(angle) + centeredCoord.y * cos(angle);
+    rotatedCoord += 0.5;
+    
+    color = texture(samp, rotatedCoord);
+}
