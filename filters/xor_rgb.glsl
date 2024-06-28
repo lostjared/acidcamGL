716c4e8d15f86fc51fb0c1c2f88ba407025a4e83
Jared Bruni <jaredbruni@gmail.com>
2024-06-27 23:59:35 -0700

    xor_rgb

---
+++ filters/xor_rgb.glsl
@@ -0,0 +1,30 @@
+#version 330
+in vec2 tc;
+out vec4 color;
+uniform sampler2D samp;
+uniform float time_f;
+uniform vec2 iResolution;
+
+vec4 xor_RGB(vec4 icolor, vec4 source) {
+    ivec3 int_color;
+    ivec4 isource = ivec4(source * 255);
+    for(int i = 0; i < 3; ++i) {
+        int_color[i] = int(255 * icolor[i]);
+        int_color[i] = int_color[i]^isource[i];
+        if(int_color[i] > 255)
+            int_color[i] = int_color[i]%255;
+        icolor[i] = float(int_color[i])/255;
+    }
+    return icolor;
+}
+
+float pingPong(float x, float length) {
+    float modVal = mod(x, length * 2.0);
+    return modVal <= length ? modVal : length * 2.0 - modVal;
+}
+
+void main(void) {
+    vec4 tcolor = texture(samp, tc);
+    float time_t = pingPong(time_f, 10.0) + 1.0;
+    color = xor_RGB(tcolor, tcolor * time_t);
+}
