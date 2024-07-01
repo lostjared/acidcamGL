800a33173d46965664d6bae7e6691e9451e8c581
Jared Bruni <jaredbruni@gmail.com>
2024-07-01 00:23:24 -0700

    bend

---
+++ filters/bend.glsl
@@ -0,0 +1,27 @@
+#version 330 core
+out vec4 color;
+in vec2 tc;
+
+uniform sampler2D samp;
+uniform float time_f;
+uniform vec2 iResolution;
+
+float pingPong(float x, float length) {
+    float modVal = mod(x, length * 2.0);
+    return modVal <= length ? modVal : length * 2.0 - modVal;
+}
+
+void main(void) {
+    vec2 uv = tc;
+    float t = mod(time_f, 10.0) * 0.1;
+    float angle = sin(t * 3.14159265) * 0.5;
+    vec2 center = vec2(0.5, 0.5);
+    uv -= center;
+    float dist = length(uv);
+    float bend = sin(dist * 6.0 - t * 2.0 * 3.14159265) * 0.05;
+    uv = mat2(cos(angle), -sin(angle), sin(angle), cos(angle)) * uv;
+    uv += center;
+    float time_t = pingPong(time_f, 10.0);
+    uv += sin(bend * uv * time_t);
+    color = texture(samp, uv);
+}
