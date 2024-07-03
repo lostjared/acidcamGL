c167507c9a11f105ea0f9790037d66b86120ed34
Jared Bruni <jaredbruni@gmail.com>
2024-07-02 20:43:14 -0700

    page turn 2

---
+++ filters/page_turn2.glsl
@@ -0,0 +1,25 @@
+#version 330 core
+out vec4 color;
+in vec2 tc;
+
+uniform sampler2D samp;
+uniform float time_f;
+uniform vec2 iResolution;
+
+float pingPong(float x, float length) {
+    float modVal = mod(x, length * 2.0);
+    return modVal <= length ? modVal : length * 2.0 - modVal;
+}
+
+void main(void) {
+    float time_t = pingPong(time_f, 10.0) + 2.0;
+    float foldAmount = sin(time_t) * 0.5 + 0.5;
+    vec2 uv = tc;
+
+    if (uv.x > foldAmount) {
+        float offset = (uv.x - foldAmount) / (1.0 - foldAmount);
+        uv.x = foldAmount + offset * foldAmount;
+    }
+
+    color = texture(samp, uv);
+}
