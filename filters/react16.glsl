844ac6e2175d054525856cf461a4f39cf5aa855e
Jared Bruni <jaredbruni@gmail.com>
2024-07-09 00:17:11 -0700

    16

---
+++ filters/react16.glsl
@@ -0,0 +1,24 @@
+#version 330 core
+out vec4 color;
+in vec2 tc;
+uniform sampler2D samp;
+uniform vec2 iResolution;
+uniform float time_f;
+
+void main(void) {
+    vec2 normPos = (gl_FragCoord.xy / iResolution.xy) * 2.0 - 1.0;
+    float dist = length(normPos);
+    float phase = sin(dist * 10.0 - time_f * 4.0);
+    vec2 tcAdjusted = tc + (normPos * 0.305 * phase);
+    vec4 color1 = texture(samp, tcAdjusted);
+    vec2 centeredCoord = (tc * 2.0) - vec2(1.0, 1.0);
+    float stretchFactor = 1.0 + (1.0 - abs(centeredCoord.y)) * 0.5;
+    centeredCoord.x *= sin(stretchFactor * time_f);
+    vec2 stretchedCoord = (centeredCoord + vec2(1.0, 1.0)) / 2.0;
+    vec4 color2 = texture(samp, stretchedCoord);
+
+    float glitchFactor = sin(time_f * 50.0);
+    vec4 glitchColor = texture(samp, tc + vec2(glitchFactor * 0.1, glitchFactor * 0.1));
+    
+    color = mix(mix(color1, color2, 0.5), glitchColor, 0.5);
+}
