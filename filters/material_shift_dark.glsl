ef7c6865900b1875178f645157a94771aca553e0
Jared Bruni <jaredbruni@gmail.com>
2024-07-17 07:56:53 -0700

    material_shift_dark

---
+++ filters/material_shift_dark.glsl
@@ -0,0 +1,67 @@
+#version 330
+
+in vec2 tc;
+out vec4 color;
+
+uniform sampler2D samp;
+uniform sampler2D mat_samp;
+uniform float time_f;
+uniform vec2 iResolution;
+
+vec4 blur(sampler2D image, vec2 uv, vec2 resolution) {
+    vec2 texelSize = 1.0 / resolution;
+    vec4 result = vec4(0.0);
+    float kernel[100];
+    float kernelVals[100] = float[](0.5, 1.0, 1.5, 2.0, 2.5, 2.5, 2.0, 1.5, 1.0, 0.5,
+                                    1.0, 2.0, 2.5, 3.0, 3.5, 3.5, 3.0, 2.5, 2.0, 1.0,
+                                    1.5, 2.5, 3.0, 3.5, 4.0, 4.0, 3.5, 3.0, 2.5, 1.5,
+                                    2.0, 3.0, 3.5, 4.0, 4.5, 4.5, 4.0, 3.5, 3.0, 2.0,
+                                    2.5, 3.5, 4.0, 4.5, 5.0, 5.0, 4.5, 4.0, 3.5, 2.5,
+                                    2.5, 3.5, 4.0, 4.5, 5.0, 5.0, 4.5, 4.0, 3.5, 2.5,
+                                    2.0, 3.0, 3.5, 4.0, 4.5, 4.5, 4.0, 3.5, 3.0, 2.0,
+                                    1.5, 2.5, 3.0, 3.5, 4.0, 4.0, 3.5, 3.0, 2.5, 1.5,
+                                    1.0, 2.0, 2.5, 3.0, 3.5, 3.5, 3.0, 2.5, 2.0, 1.0,
+                                    0.5, 1.0, 1.5, 2.0, 2.5, 2.5, 2.0, 1.5, 1.0, 0.5);
+    
+    for (int i = 0; i < 100; i++) {
+        kernel[i] = kernelVals[i];
+    }
+
+    float kernelSum = 0.0;
+    for (int i = 0; i < 100; i++) {
+        kernelSum += kernel[i];
+    }
+
+    for (int x = -5; x <= 4; ++x) {
+        for (int y = -5; y <= 4; ++y) {
+            vec2 offset = vec2(float(x), float(y)) * texelSize;
+            result += texture(image, uv + offset) * kernel[(y + 5) * 10 + (x + 5)];
+        }
+    }
+
+    return result / kernelSum;
+}
+
+float pingPong(float x, float length) {
+    float modVal = mod(x, length * 2.0);
+    return modVal <= length ? modVal : length * 2.0 - modVal;
+}
+
+vec4 colorRotate(vec4 color) {
+    ivec4 col = ivec4(color * 255);
+    for(int i = 0; i < 3; ++i) {
+        col[i] = col[i]%0xFF;
+    }
+    return vec4(col/255.0);
+}
+
+void main() {
+    vec4 col = blur(samp, tc, iResolution);
+    vec4 m_col = blur(mat_samp, tc, iResolution);
+    float value = pingPong(time_f, 10.0) + 2.0;
+    float time_t =  pingPong(time_f, 7.0) + 2.0;
+    col = mix(col, m_col, 0.5);
+    color = sin(colorRotate(col * value) * time_t);
+}
+
+
