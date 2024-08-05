865d5624f2938dd931894d6e458e499e6a2be397
Jared Bruni <jaredbruni@gmail.com>
2024-08-05 00:40:36 -0700

    updated new shader cwave

---
+++ filters/cwave.glsl
@@ -0,0 +1,17 @@
+#version 330 core
+out vec4 color;
+in vec2 tc;
+
+uniform sampler2D samp;
+uniform float time_f;
+uniform vec2 iResolution;
+
+void main(void) {
+    vec2 uv = tc * iResolution;
+    uv -= 0.5 * iResolution;
+    float dist = length(uv);
+    float angle = atan(uv.y, uv.x);
+    float wave = sin(dist * 10.0 - time_f * 5.0 + angle * 5.0);
+    vec4 texColor = texture(samp, tc);
+    color = texColor * (0.5 + 0.5 * wave);
+}
