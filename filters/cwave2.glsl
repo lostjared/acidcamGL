e3f6e2aaa43e1b2ac4ea8fb364602175ad8682e8
Jared Bruni <jaredbruni@gmail.com>
2024-08-05 22:49:01 -0700

    pingPong

--- filters/cwave2.glsl
+++ filters/cwave2.glsl
@@ -6,14 +6,19 @@ uniform sampler2D samp;
 uniform float time_f;
 uniform vec2 iResolution;
 
+float pingPong(float x, float length) {
+    float modVal = mod(x, length * 2.0);
+    return modVal <= length ? modVal : length * 2.0 - modVal;
+}
+
 void main(void) {
     vec2 uv = tc * iResolution;
     uv -= 0.5 * iResolution;
     float dist = length(uv);
     float angle = atan(uv.y, uv.x);
-    
+    float time_t = pingPong(time_f, 20.0);
     float wave = sin(dist * 10.0 - time_f * 5.0 + angle * 5.0);
-    wave = sin(wave * time_f);
+    wave = sin(wave * time_t);
     
     vec4 texColor = texture(samp, tc);
     
