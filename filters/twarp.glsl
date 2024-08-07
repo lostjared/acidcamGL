a44a5837c2b2cc28e4ff58f879f742530d3b830d
Jared Bruni <jaredbruni@gmail.com>
2024-08-07 10:40:07 -0700

    shader

---
+++ filters/twarp.glsl
@@ -0,0 +1,17 @@
+#version 330 core
+out vec4 color;
+in vec2 tc;
+
+uniform sampler2D samp;
+uniform float time_f;
+uniform vec2 iResolution;
+
+void main(void) {
+    vec2 uv = tc;
+    float warpX = tan(uv.y * 10.0 + time_f) * 0.1;
+    float warpY = tan(uv.x * 10.0 + time_f) * 0.1;
+    uv.x += warpX;
+    uv.y += warpY;
+    color = texture(samp, uv);
+}
+
