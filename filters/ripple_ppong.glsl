9f35cc87a4b8c53c1f86da9162d5f401dffdaf28
Jared Bruni <jaredbruni@gmail.com>
2024-08-23 15:36:09 -0700

    ripple

---
+++ filters/ripple_ppong.glsl
@@ -0,0 +1,25 @@
+#version 330 core
+out vec4 color;
+in vec2 tc;
+
+uniform sampler2D samp;
+uniform float time_f;
+uniform vec2 iResolution;
+
+float pingPong(float x, float length) {
+    float modVal = mod(x, length * 2.0);
+    return modVal <= length ? modVal : length * 2.0 - modVal;
+}
+
+void main(void) {
+    vec2 center = iResolution / 2.0;
+    vec2 uv = tc * iResolution;
+    float distanceFromCenter = length(uv - center);
+    float rippleSpeed = 2.0;
+    float rippleFrequency = 20.0;
+    float time = time_f * rippleSpeed;
+    float wave = sin(pingPong(distanceFromCenter + time, rippleFrequency));
+
+    vec2 rippleUV = tc + normalize(uv - center) * wave * 0.01;
+    color = texture(samp, rippleUV);
+}
