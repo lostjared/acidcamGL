95d4d41f636cd9c07b370a08aab9dc43c2e9ee91
Jared Bruni <jaredbruni@gmail.com>
2024-08-23 15:39:59 -0700

    updated

--- filters/ripple_ppong.glsl
+++ filters/ripple_ppong.glsl
@@ -15,11 +15,18 @@ void main(void) {
     vec2 center = iResolution / 2.0;
     vec2 uv = tc * iResolution;
     float distanceFromCenter = length(uv - center);
+
     float rippleSpeed = 2.0;
     float rippleFrequency = 20.0;
+
     float time = time_f * rippleSpeed;
     float wave = sin(pingPong(distanceFromCenter + time, rippleFrequency));
 
-    vec2 rippleUV = tc + normalize(uv - center) * wave * 0.01;
+    float warpStrength = 0.05;
+    float warpFrequency = 5.0;
+    vec2 warp = vec2(sin(uv.y * warpFrequency + time), sin(uv.x * warpFrequency + time)) * warpStrength;
+
+    vec2 rippleUV = tc + normalize(uv - center) * wave * 0.01 + warp;
+
     color = texture(samp, rippleUV);
 }
