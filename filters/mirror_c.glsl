5bc0c52ed5e9b32d156af459e91de5af60b2b673
Jared Bruni <jaredbruni@gmail.com>
2024-11-29 20:22:30 -0800

    mirror shader

---
+++ filters/mirror_c.glsl
@@ -0,0 +1,30 @@
+#version 330 core
+out vec4 color;
+in vec2 tc;
+
+uniform sampler2D samp;
+uniform vec2 iResolution;
+uniform float time_f;
+
+mat2 rotate(float angle) {
+    float c = cos(angle);
+    float s = sin(angle);
+    return mat2(c, -s, s, c);
+}
+
+vec2 mirror(vec2 uv) {
+    return abs(mod(uv, 2.0) - 1.0);
+}
+
+void main() {
+    vec2 uv = tc * iResolution.xy;
+    vec2 sectionSize = vec2(100.0);
+    vec2 sectionIndex = floor(uv / sectionSize);
+    vec2 localUV = mod(uv, sectionSize) / sectionSize;
+    float angle = time_f + length(sectionIndex) * 0.5;
+    localUV = rotate(angle) * (localUV - 0.5) + 0.5;
+    localUV = mirror(localUV);
+    vec2 texCoord = (sectionIndex + localUV) * sectionSize / iResolution.xy;
+    color = texture(samp, texCoord);
+}
+
