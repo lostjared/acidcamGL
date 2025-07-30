63232c6211bb0156bbec2b9a4e5ee95d9afcd70f
Jared Bruni <jaredbruni@gmail.com>
2025-07-30 04:36:51 -0700

    fix

--- source/ffmpeg_write.cpp
+++ source/ffmpeg_write.cpp
@@ -39,7 +39,7 @@ FILE *open_ffmpeg(const char *output, const char *codec, const char *res, const 
 
     // Get directory
     std::string dir = dirname(exePath);
-    ffmpeg_path = dir + "/" + filename;
+    ffmpeg_path = dir + "/ffmpeg";;
 
     #endif
 
